d8c3806b5114c0ed047aeffbd867257f3a59521b
08DD <ddybroff@gmail.com>
2023-10-08 13:00:23 +0300

    написал инит для Компайлера

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,11 +1,10 @@
-import Invoker
+class Compiler:
+    def __init__(self, file_to_compile, name_of_file):
+        self.file = file_to_compile
+        self.ext = name_of_file.split(".")[-1]
+        self.commands = {'py': 'something',
+                         'cpp': 'g++'}
 
 
-file = input()
-ext = input()
-
-compilers = {'py': 'something',
-             'cpp': 'g++'}
-
-
-print("g")
+a = Compiler("print(f)", "print.py")
+print("f")
