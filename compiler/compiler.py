3ce7e7539e5b8cc0fa5f3842a780f428b97e4d96
08DD <ddybroff@gmail.com>
2023-10-08 14:52:40 +0300

    Доработал вид результата, дополнил про забытый InvokerReport.

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,6 +1,8 @@
+import InvokerReport
 import InvokerRequest
 import InvokerMultiRequest
 import subprocess
+
 
 class Compiler:
     def __init__(self, file_to_compile, file_name, id_of_file):
@@ -8,21 +10,28 @@ class Compiler:
         self.ext = file_name.split(".")[-1]
         self.id = id_of_file
         self.compiled_file = ""
+        self.results = []
+        self.report = InvokerReport()
         self.commands = {'py': 'something',
                          'cpp': f"g++ -c {self.file} -o C:\\Users\\ddybr\\Desktop\\Compiled_files\\{self.id}"}
         self.compile()
+        self.compiled_file = open(f"C:\\Users\\ddybr\\Desktop\\Compiled_files\\{self.id}")
+        self.results.append(self.compiled_file)
+        self.results.append(self.report)
 
     def do(self):
         subprocess.Popen(self.commands[self.ext])
 
     def compile(self):
-        ireq = InvokerRequest(self.commands[self.ext])
+        ireq = InvokerRequest(self.commands[self.ext], self.results, True)
         listireqs = [self.ireq]
         imr = InvokerMultiRequest(self.listireqs, "Compiler", 10)
         imr.run()
 
     def get_results(self):
-        compiled_file = open(f"C:\\Users\\ddybr\\Desktop\\Compiled_files\\{self.id}")
+        return self.results
+
+
 
 
 a = Compiler("C:\\Users\\ddybr\\ciplusplus\\main.cpp", "main.cpp", 1)
