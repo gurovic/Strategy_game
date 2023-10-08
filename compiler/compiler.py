0b2c17a8c320ae9d1b022831292d5ceefe6f2f12
08DD <ddybroff@gmail.com>
2023-10-08 14:37:47 +0300

    первая версия Compiler

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,10 +1,29 @@
+import InvokerRequest
+import InvokerMultiRequest
+import subprocess
+
 class Compiler:
-    def __init__(self, file_to_compile, name_of_file):
+    def __init__(self, file_to_compile, file_name, id_of_file):
         self.file = file_to_compile
-        self.ext = name_of_file.split(".")[-1]
+        self.ext = file_name.split(".")[-1]
+        self.id = id_of_file
+        self.compiled_file = ""
         self.commands = {'py': 'something',
-                         'cpp': 'g++'}
+                         'cpp': f"g++ -c {self.file} -o C:\\Users\\ddybr\\Desktop\\Compiled_files\\{self.id}"}
+        self.compile()
+
+    def do(self):
+        subprocess.Popen(self.commands[self.ext])
+
+    def compile(self):
+        ireq = InvokerRequest(self.commands[self.ext])
+        listireqs = [self.ireq]
+        imr = InvokerMultiRequest(self.listireqs, "Compiler", 10)
+        imr.run()
+
+    def get_results(self):
+        compiled_file = open(f"C:\\Users\\ddybr\\Desktop\\Compiled_files\\{self.id}")
 
 
-a = Compiler("print(f)", "print.py")
+a = Compiler("C:\\Users\\ddybr\\ciplusplus\\main.cpp", "main.cpp", 1)
 print("f")
