5ef5242cfbe56b3bb6dccd98fe0d81c625b91cbd
Sasha G <a1exgo111v@gmail.com>
2023-10-09 21:45:35 +0300

    models __init__ Edited

--- website/app/models/__init__.py
+++ website/app/models/__init__.py
@@ -1,3 +1,3 @@
-from model_game import Game
-from model_battle import Battle, PlayersInBattles
-from model_tournament import Tournament, PlayersInTournament
+from .model_game import Game
+from .model_battle import Battle, PlayersInBattles
+from .model_tournament import Tournament, PlayersInTournament
