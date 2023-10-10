e677d58cf7b642545fdb2e861bb5d4aa674a4812
ilgizar <vladaboom111@gmail.com>
2023-10-10 21:56:13 +0300

    Sample for the tournament

--- website/app/admin.py
+++ website/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(Tournament)
+admin.site.register(Game)
+admin.site.register(PlayersInBattles)
+admin.site.register(PlayersInTournament)
+admin.site.register(Battle)
