241cbc8a604290828d468a9204fedd4fe235a411
08DD <ddybroff@gmail.com>
2023-10-22 14:47:18 +0300

    отредактировал код

--- website/app/compiler.py
+++ website/app/compiler.py
@@ -1,54 +1,57 @@
-import logging
-import time
-import InvokerReport
 import InvokerRequest
 import InvokerMultiRequest
 import InvokerMultiRequestPriorityQueue
-import random
-import subprocess
-import PRIORITY
 from models import model_compiler_report
-logging.basicConfig(filename="log_of_compiler.txt", format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level=logging.DEBUG)
 
 
 class Compiler:
+    import logging
+    logging.basicConfig(filename="log_of_compiler.txt",
+                        format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                        level=logging.DEBUG)
+
     COMMANDS = {'py': None,
-                'cpp': "g++ -c {} -o Compiled_files/{}"}
-    WHICHFILEISCOMPILING = "{} with id {} is compiling on {}"
-    FILEISCOMPILED = "{} with id {} is compiled and saved in Compiled_files/{}"
+                'cpp': "g++ -c {} -o {}"}
+    WHICHFILEISCOMPILING = "{} with id {} is compiling on {}{}"
+    FILEISCOMPILED = "{} with id {} is compiled and saved in {}{}"
     FILEISNOTCOMPILED = "{} with id {} is not compiled with error {}"
     FILETOCOMPILEISCREATED = "{} with id {} is created"
+    WHOCREATED = "{} with id {} is created by {}"
 
-    def __init__(self, path_to_file, who_needs_compile):
-        self.who_needs = who_needs_compile
-        self.file = path_to_file
+    def __init__(self, path_to_file, class_who_is_using, where_to_save):
+        from random import randint
+        self.clas = class_who_is_using
+        self.path = path_to_file
         self.extension = path_to_file.split(".")[-1]
-        self.id = random.randint(0, int(1e12))
+        self.id = randint(0, int(1e12))
+        self.where = where_to_save
         if self.COMMANDS[self.extension] is not None:
-            self.command = self.COMMANDS[self.extension].format(self.file, self.id)
-        logging.info(self.FILETOCOMPILEISCREATED.format(self.file, self.id))
+            self.command = self.COMMANDS[self.extension].format(self.path, self.where, self.id)
+        self.logging.info(self.FILETOCOMPILEISCREATED.format(self.path, self.id))
+        self.logging.info(self.WHOCREATED.format(self.path, self.id, self.clas))
 
     def compile(self):
-        logging.info(self.WHICHFILEISCOMPILING.format(self.file, self.id))
-        self.report = InvokerReport()
-        invoker_request = InvokerRequest(self.command, self.report, True)
+        import PRIORITY
+        self.logging.info(self.WHICHFILEISCOMPILING.format(self.path, self.id))
+        invoker_request = InvokerRequest(self.command, True)
         invoker_requests = [invoker_request]
-        invoker_multirequest = InvokerMultiRequest(invoker_requests, self, PRIORITY.RED)
+        invoker_multirequest = InvokerMultiRequest(invoker_requests, self)
         queue = InvokerMultiRequestPriorityQueue()
-        queue.add(invoker_multirequest)
+        queue.add(invoker_multirequest, PRIORITY.RED)
 
-    def notify(self):
-        compile_report = model_compiler_report.CompilerReport
-        compile_report.id = "id of compiled file is {}".format(self.id)
-        compile_report.time = "at {} started, at {} ended".format(self.report.time_start, self.report.time_end)
-        compile_report.compile_status = self.report.compile_status
-        compile_report.compile_error_text = self.report.compile_error_text
-        if self.report.compile_status == 1:
-            self.who_needs.notify()
-            logging.info(self.FILEISCOMPILED.format(self.file, self.id, self.id))
+    def notify(self, report):
+        compile_report = model_compiler_report.CompilerReport()
+        compile_report.id = self.id
+        compile_report.path = self.path
+        compile_report.time = "at {} started, at {} ended".format(report.time_start, report.time_end)
+        compile_report.compile_status = report.compile_status
+        compile_report.compile_error_text = report.compile_error_text
+        if report.compile_status == 1:
+            self.clas.notify()
+            self.logging.info(self.FILEISCOMPILED.format(self.path, self.id, self.where, self.id))
         else:
-            self.who_needs.notify()
-            logging.error(self.FILEISNOTCOMPILED.format(self.file, self.id, self.report.compile_status))
+            self.clas.notify()
+            self.logging.error(self.FILEISNOTCOMPILED.format(self.path, self.id, report.compile_status))
 
 
 
--- website/app/models/model_compiler_report.py
+++ website/app/models/model_compiler_report.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 class CompilerReport(models.Model):
     id_of_compiled_file = models.IntegerField(max_length=60)
+    path_of_compiled_file = models.CharField(max_length=60)
     time = models.CharField(max_length=60)
     compile_status = models.CharField(max_length=60)
     compile_error_text = models.CharField(max_length=60)
