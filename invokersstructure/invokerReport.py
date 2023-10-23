3cf00facd0bf0b4deb3e3eee1b6d2964603e0bb4
DerwesX <2025lobanov.ss@student.letovo.ru>
2023-10-23 16:16:27 +0300

    Report added

--- invokersstructure/invokerReport.py
+++ invokersstructure/invokerReport.py
@@ -1,7 +1,23 @@
-# Created at 2023/10/15
+class InvokerReport():
 
-class InvokerReport:
+    def __init__(self):
+        self.time_start = None
+        self.time_end = None
+        self.program_status = None
+        self.program_error_text = None
 
-    def __init__(self, report):
-        self.exitcode = report
-        self.exitreport = report.read()
+    def add_values(self, time_start, time_end, program_status, program_eror_text):
+        self.time_start = time_start
+        self.time_end = time_end
+        self.program_status = program_status
+        self.program_error_text = program_eror_text
+
+    def add_single_value(self, value, type):
+        if (type == "time_start"):
+            self.time_start = value
+        if (type == "time_end"):
+            self.time_end = value
+        if (type == "program_status"):
+            self.program_status = value
+        if (type == "program_error_text"):
+            self.program_error_text = value
