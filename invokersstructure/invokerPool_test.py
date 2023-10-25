89a3af1c4524caa57e877b76b6a704b1db622429
Palov Dmitriy <palov.dmitriy@yandex.ru>
2023-10-25 13:01:07 +0300

    test

--- invokersstructure/invokerPool_test.py
+++ invokersstructure/invokerPool_test.py
@@ -5,3 +5,5 @@ class TestInvokerPool(unittest.testCase):
     def test_count(self):
         in_p = InvokerPool
         self.assertEqual(in_p.ALL_INVOKERS_COUNT,100)
+    def test_function(self):
+        pass
