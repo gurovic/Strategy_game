e82dd3d2e67a4c945005b14f91086af176f3a573
Palov Dmitriy <palov.dmitriy@yandex.ru>
2023-10-25 12:36:25 +0300

    Checking if branch is working

---
+++ invokersstructure/invokerPool_test.py
@@ -0,0 +1,8 @@
+import unittest
+from invokerPool import InvokerPool
+
+class TestInvokerPool(unittest.testCase):
+    def test_count(self):
+        in_p = InvokerPool
+        self.assertEqual(in_p.ALL_INVOKERS_COUNT,100)
+
