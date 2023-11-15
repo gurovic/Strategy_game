04f20e88404872a0b74c4038c25563d3fa3e4120
MrNereof <mrnereof@gmail.com>
2023-11-15 13:00:32 +0300

    Update fields of CompilerReport

--- website/app/models/compiler.py
+++ website/app/models/compiler.py
@@ -5,11 +5,13 @@ class CompilerReport(models.Model):
 
     class Status(models.IntegerChoices):
         OK = 0
-        ERROR = 1
+        COMPILER_ERROR = 1
+        COMPILATION_ERROR = 2
+        TIMELIMIT = 3
 
     compiled_file = models.FileField(upload_to="compiler_report", null=True, blank=True, verbose_name="Файл")
-    time = models.PositiveIntegerField(verbose_name="Время выполнения")
-    date_created = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time = models.DurationField(verbose_name="Время выполнения")
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     status = models.IntegerField(choices=Status.choices, default=Status.OK, verbose_name="Статус")
     error = models.TextField(editable=False, blank=True, null=True, verbose_name="Ошибка")
     invoker_report = models.ForeignKey("app.InvokerReport", on_delete=models.CASCADE, verbose_name="Репорт инвокера")
@@ -18,6 +20,9 @@ class CompilerReport(models.Model):
         if self.status == self.Status.OK:
             return f"{self.id} - {self.get_status_display()}"
         return f"{self.id} - {self.get_status_display()} | {self.error}"
+
+    def has_error(self):
+        return self.status != self.Status.OK
 
     class Meta:
         verbose_name = "Репорт компилятора"
--- website/app/models/compiler_test.py
+++ website/app/models/compiler_test.py
@@ -5,10 +5,28 @@ from app.models import CompilerReport
 class CompilerReportTestCase(TestCase):
     def setUp(self):
         CompilerReport.objects.create(status=CompilerReport.Status.OK)
-        CompilerReport.objects.create(status=CompilerReport.Status.ERROR, error="Test")
+        CompilerReport.objects.create(status=CompilerReport.Status.COMPILER_ERROR, error="Test")
+        CompilerReport.objects.create(status=CompilerReport.Status.COMPILATION_ERROR, error="Test")
+        CompilerReport.objects.create(status=CompilerReport.Status.TIMELIMIT, error="Test")
 
     def test_str(self):
         ok = CompilerReport.objects.get(status=CompilerReport.Status.OK)
-        error = CompilerReport.objects.get(status=CompilerReport.Status.ERROR)
+        compiler_error = CompilerReport.objects.get(status=CompilerReport.Status.COMPILER_ERROR)
+        compilation_error = CompilerReport.objects.get(status=CompilerReport.Status.COMPILATION_ERROR)
+        timelimit_error = CompilerReport.objects.get(status=CompilerReport.Status.TIMELIMIT)
+
         self.assertEquals(str(ok), f"{ok.id} - {ok.get_status_display()}")
-        self.assertEquals(str(error), f"{error.id} - {error.get_status_display()} | {error.error}")
+        self.assertEquals(str(compiler_error), f"{compiler_error.id} - {compiler_error.get_status_display()} | {compiler_error.error}")
+        self.assertEquals(str(compilation_error), f"{compilation_error.id} - {compilation_error.get_status_display()} | {compilation_error.error}")
+        self.assertEquals(str(timelimit_error), f"{timelimit_error.id} - {timelimit_error.get_status_display()} | {timelimit_error.error}")
+
+    def test_has_error(self):
+        ok = CompilerReport.objects.get(status=CompilerReport.Status.OK)
+        compiler_error = CompilerReport.objects.get(status=CompilerReport.Status.COMPILER_ERROR)
+        compilation_error = CompilerReport.objects.get(status=CompilerReport.Status.COMPILATION_ERROR)
+        timelimit_error = CompilerReport.objects.get(status=CompilerReport.Status.TIMELIMIT)
+
+        self.assert_(ok.has_error())
+        self.assert_(not compiler_error.has_error())
+        self.assert_(not compilation_error.has_error())
+        self.assert_(not timelimit_error.has_error())
