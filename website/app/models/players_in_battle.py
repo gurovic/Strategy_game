90f1d03d7f2be19197de682e4bc067edb82af8b0
boriskiva <boriskiva3@gmail.com>
2023-11-16 10:28:26 +0300

    adding PlayersInBattle class

---
+++ website/app/models/players_in_battle.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class PlayersInBattle(models.Model):
+    path = models.TextField(editable=False, blank=True, null=True, verbose_name="Путь к файлу")
+    strategy_id = models.IntegerField(default=0, verbose_name="id стратегии в Battle")
+    is_winner = models.BooleanField(default=False, verbose_name="Победитель")
