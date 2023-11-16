7790070632725f2c47e1def925eef55fd51a836e
MrNereof <mrnereof@gmail.com>
2023-11-16 23:52:15 +0300

    Add tests for InvokerRequest

---
+++ website/invoker/test_invoker.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from unittest.mock import patch, Mock
+
+from invoker.invoker import Invoker
+
+
+class TestInvoker(TestCase):
+    @patch("invoker.models.InvokerReport")
+    def test_notify(self, mock_invoker_report):
+        mock = Mock()
+        invoker = Invoker()
+        invoker.run("echo Hello World", callback=mock)
+        mock.assert_called()
