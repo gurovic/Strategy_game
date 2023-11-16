06536068a8b167db8bf6fd006a3b89ab12fe355f
boriskiva <boriskiva3@gmail.com>
2023-11-16 22:18:55 +0300

    saving BattleReport to a django model

---
+++ website/app/models/battle_report.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class BattleReport(models.Model):
+    invoker_report = models.ForeignKey('app.InvokerReport', on_delete=models.CASCADE, verbose_name='InvokerReport')
+    battle_id = models.IntegerField(default=0, verbose_name='Battle id')
