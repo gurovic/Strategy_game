5fffc72e40f469fa7516b2f0d0156504f73b181c
MrNereof <mrnereof@gmail.com>
2023-11-17 09:55:53 +0300

    Fix InvokerMultiRequest

--- website/invoker/invoker_multi_request.py
+++ website/invoker/invoker_multi_request.py
@@ -28,14 +28,14 @@ class InvokerMultiRequest:
     def start(self):
         from invoker.invoker_multi_request_priority_queue import InvokerMultiRequestPriorityQueue
         invoker_pq = InvokerMultiRequestPriorityQueue()
-        invoker_pq.add(self, self.priority)
+        invoker_pq.add(self)
 
     def run(self, invokers):
         for (invoker, invoker_request) in zip(invokers, self.invoker_requests):
             invoker_request.run(invoker)
 
     def notify(self, invoker_report: InvokerReport):
-        self.invoker_requests_count += 1
+        self.invoker_request_ended += 1
         self.claimed_reports.append(invoker_report)
         if self.invoker_request_ended >= self.invoker_requests_count:
             for subscriber in self.subscribers:
