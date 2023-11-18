8f473d1a604c44d0feeb049f53495f7fb1d1c3ea
Ilgizar <instasamka@gmail.com>
2023-11-19 00:38:00 +0300

    invoker_report - unittest3

---
+++ website/app/models/test_invoker_report.py
@@ -0,0 +1,19 @@
+import django.test
+from invokersstructure.invoker_report import *
+
+
+class TestInvokerReport(django.test.TestCase):
+    def test_different(self):
+        a = InvokerReport
+        b = InvokerReport
+        self.assertEqual(a, b)
+
+    def test_origin(self):
+        a = InvokerReport
+        self.assertEqual(a.invoker_id, -1)
+        self.assertEqual(a.program_type, '')
+        self.assertEqual(a.running_file, None)
+        self.assertEqual(a.time_start, None)
+        self.assertEqual(a.time_end, None)
+        self.assertEqual(a.program_status, STATUS["NS"])
+        self.assertEqual(a.program_error_text, None)
