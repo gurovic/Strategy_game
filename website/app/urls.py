a74446d1fed756d174cf4937540c7f2c3e9f8176
boriskiva <boriskiva3@gmail.com>
2023-11-23 22:52:35 +0300

    making sandbox_views

---
+++ website/app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import sandbox_views
+
+urlpatterns = [
+    path('sandbox/<int:id>', sandbox_views.show),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
