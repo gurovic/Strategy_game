236c940a16a2b65d029291f87426249bc428f11e
boriskiva <boriskiva3@gmail.com>
2023-11-23 22:34:27 +0300

    making sandbox views

--- website/app/urls.py
+++ website/app/urls.py
@@ -1,12 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from .views import sandbox_views
 
-    path('app/', include('app.urls')),
+urlpatterns = [
+    path('sandbox/<int:id>', sandbox_views.show),
 ]
 
 if settings.DEBUG:
---
+++ website/app/views/sandbox_views.py
@@ -0,0 +1,23 @@
+from ..models.game import Game
+from ..models import CompilerReport
+from ..classes import FileLoader, save_file, Sandbox
+
+from django.shortcuts import render
+
+
+def compile(file):
+    path = save_file(file)
+    file_loader = FileLoader(path)
+    compiler_report_id = file_loader.get_compiler_report_id()
+    compiler_report = CompilerReport.objects.get(pk=compiler_report_id)
+    return compiler_report
+
+
+def show(request,id):
+    if request.method == 'POST':
+        strategy = compile(request.FILES['strategy'])
+        game = Game.objects.get(pk=id)
+        sandbox = Sand
+        return render(request, "sandbox_views.html", {"report": report})
+    else:
+        return
