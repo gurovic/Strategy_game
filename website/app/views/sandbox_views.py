15c05f76020d71f682cd02d6b37b57662ec3cd78
boriskiva <boriskiva3@gmail.com>
2023-11-23 22:53:14 +0300

    making sandbox_views

--- website/app/views/sandbox_views.py
+++ website/app/views/sandbox_views.py
@@ -1,4 +1,17 @@
+from ..models.game import Game
+from ..models import CompilerReport
+from ..classes import FileLoader, save_file
 
+from django.shortcuts import render
+from django import forms
+
+
+def compile(file):
+    path = save_file(file)
+    file_loader = FileLoader(path)
+    compiler_report_id = file_loader.get_compiler_report_id()
+    compiler_report = CompilerReport.objects.get(pk=compiler_report_id)
+    return compiler_report
 
 
 def show():
