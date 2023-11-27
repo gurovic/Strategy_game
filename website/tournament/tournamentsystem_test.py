fa1ad0a9b86db68b9f1a846b4e0f18dd80f0d0e2
08DD <ddybroff@gmail.com>
2023-11-27 16:25:13 +0300

    tour

--- website/tournament/tournamentsystem_test.py
+++ website/tournament/tournamentsystem_test.py
@@ -1,9 +1,22 @@
 import unittest
+from unittest.mock import Mock, patch
+from tournamentsystem import TournamentSystem
+from tournament.tournament import Tournament
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestTournamentSystem(unittest.TestCase):
+    @patch("tournament.tournament.Tournament")
+    def test_init(self):
+        b = Tournament
+        a = TournamentSystem(b)
+        self.assertIs(a.tournament, b)
+        self.assertListEqual([], a.battles)
+
+    def test_notify(self):
+        mock = Mock()
+        a = TournamentSystem(mock)
+        a.notify()
+        mock.assert_called()
 
 
 if __name__ == '__main__':
