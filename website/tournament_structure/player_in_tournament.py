d7323450353bd0e57bf32936ec646668b444da40
MIHAIL ZAGORODNUYK <mikle.zag@gmail.com>
2023-11-27 16:15:37 +0300

    base

---
+++ website/tournament_structure/player_in_tournament.py
@@ -0,0 +1,7 @@
+class PlayerInTournament:
+    def __init__(self, player, tournament):
+        self.player = player
+        self.tournament = tournament
+        self.number_of_points = 0
+        self.place = 0
+        self.path = ""
