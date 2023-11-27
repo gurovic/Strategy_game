172d5eac7b4432eb4973e4b75e547c982c0e731e
08DD <ddybroff@gmail.com>
2023-11-27 18:42:10 +0300

    исправил то что было в ненужных коммитах

--- website/website/urls.py
+++ website/website/urls.py
@@ -18,12 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from app.views.view_upload_game import file_upload
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('upload_game/', file_upload)
 ]
 
-#if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
