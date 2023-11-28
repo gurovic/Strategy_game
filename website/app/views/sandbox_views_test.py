66386dcb507e87047e61ca6d54cdbe8c82dd9bf4
boriskiva <boriskiva3@gmail.com>
2023-11-28 11:57:34 +0300

    fixing pull request

---
+++ website/app/views/sandbox_views_test.py
@@ -0,0 +1,11 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from unittest.mock import Mock
+
+
+class TestSandboxViews(TestCase):
+    def test_creating(self):
+        pass
+
+    def test_request(self):
+        pass
