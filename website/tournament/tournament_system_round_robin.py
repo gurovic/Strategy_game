2785f0673085bb9c65eebeb7cadf90cc75363aa9
Palov Dmitriy <palov.dmitriy@yandex.ru>
2023-12-04 15:06:15 +0300

    updated code

--- website/tournament/tournament_system_round_robin.py
+++ website/tournament/tournament_system_round_robin.py
@@ -1,24 +1,32 @@
 from tournament_system import TournamentSystem
-from app/models/battle import Battle
+from app.models.battle import Battle
+
 class TournamentSystemRoundRobin(TournamentSystem):
-    def __init__(self, tournament, battles):
-        super().__init__(tournament,battles)
+    def __init__(self, tournament):
+        super().__init__(tournament)
+        self.battle_count = len(self.tournament.players)*(len(self.tournament.players)-1)/2
 
     def run_tournament(self):
         for i in range(len(self.tournament.players)):
-            for j in range(len(self.tournament.players)):
-                if i==j:
-                    continue
-                battle = Battle(self.tournament.game, list([self.tournament.players[i],self.tournament.players[j]]))
+            for j in range(i+1, len(self.tournament.players)):
+                Battle(self.tournament.game, list([self.tournament.players[i], self.tournament.players[j]]))
 
     def calculate_places(self):
-        pass
+        places = self.tournament.players
+        places = sorted(places, key = lambda x: (x.number_of_wins, x.number_of_points))
+        self.finish()
+        return places
 
     def finish(self):
-        pass
+        self.tournament.end()
 
     def write_battle_result(self, battle):
-        pass
+        if battle.players[0].iswinner:
+            battle.players[0].number_of_wins += 1
+        elif battle.players[1].iswinner:
+            battle.players[1].number_of_wins += 1
 
-    def notify(self):
-        pass
+    def notify(self, battle):
+        self.battle_count -= 1
+        self.battles.append(battle)
+        self.write_battle_result(battle)
