541065c65e70494c8315ce546eab9c5a9278a977
MIHAIL ZAGORODNUYK <mikle.zag@gmail.com>
2023-12-04 15:33:01 +0300

    методы перенесены в ./website/tournament/player_in_tournament.py

--- website/tournament/player_in_tournament.py
+++ website/tournament/player_in_tournament.py
@@ -1,7 +1,2 @@
 class PlayerInTournament:
-    def __init__(self, player, tournament):
-        self.player = player
-        self.tournament = tournament
-        self.number_of_points = 0
-        self.place = 0
-        self.path = ""
+    pass
