aa310c437102851a8daa1952639babfa680b2932
boriskiva <boriskiva3@gmail.com>
2023-12-06 11:06:49 +0300

    fixing pull request

--- website/app/classes/sandbox.py
+++ website/app/classes/sandbox.py
@@ -1,5 +1,3 @@
-import time
-
 from .battle import Battle
 from app.models import PlayersInBattle
 
@@ -9,9 +7,9 @@ class Sandbox:
         self.game = game
         self.strategy = strategy
         players = [PlayersInBattle(player=strategy, strategy_id=0)]
-        for i in range(game.number_of_players - 1):
-            players.append(PlayersInBattle(player=game.ideal_solution, strategy_id=i + 1))
-        self.battle = Battle(game, players)
+        for i in range(self.game.number_of_players - 1):
+            players.append(PlayersInBattle(player=self.game.ideal_solution, strategy_id=i+1))
+        self.battle = Battle(self.game, players)
 
     def run_battle(self):
         self.battle.run()
--- website/app/classes/sandbox_test.py
+++ website/app/classes/sandbox_test.py
@@ -1,16 +1,67 @@
-from app.classes.sandbox import Sandbox
-from unittest.mock import Mock,patch
-
+import random
+from typing import Any
+from unittest.mock import Mock, patch, PropertyMock
 from django.test import TestCase
+
+from app.classes.sandbox import Sandbox
 
 
 class SandboxTest(TestCase):
-    @patch("app.models.Battle")
-    def test_create(self, mock_battle):
-        a =
+    @patch("app.classes.sandbox.Battle")
+    @patch("app.classes.sandbox.PlayersInBattle")
+    def test_create(self, mock_battle: Mock, mock_players_in_battle: Mock):
+        class GameMock(Mock):
+            def __init__(self, **kwargs: Any):
+                super().__init__(**kwargs)
+                self.number_of_players = 2
 
-    def test_running(self):
-        pass
+        a = Sandbox(GameMock(), None)
+        self.assertEqual(mock_battle.call_count, 2)
+        self.assertEqual(mock_players_in_battle.call_count, 1)
+        # self.assertEqual(mock_game.call_count, 3)
+        self.assertEqual(a.strategy, None)
 
-    def test_get_result(self):
-        pass
+    @patch("app.classes.sandbox.Battle")
+    @patch("app.classes.sandbox.PlayersInBattle")
+    def test_running(self, mock_battle: Mock, mock_players_in_battle: Mock):
+        class GameMock(Mock):
+            def __init__(self, **kwargs: Any):
+                super().__init__(**kwargs)
+                self.number_of_players = 2
+
+        class BattleMock(Mock):
+            def __init__(self, **kwargs: Any):
+                super().__init__(**kwargs)
+                self.count_battle = 0
+
+            def run(self):
+                self.count_battle += 1
+
+        a = Sandbox(GameMock(), None)
+        a.battle = BattleMock()
+        a.run_battle()
+        self.assertEqual(a.battle.count_battle, 1)
+
+    @patch("app.classes.sandbox.Battle")
+    @patch("app.classes.sandbox.PlayersInBattle")
+    def test_get_result(self, mock_battle: Mock, mock_players_in_battle: Mock):
+        global random_number
+        random_number = random.randint(1, 1000000000)
+
+        class GameMock(Mock):
+            def __init__(self, **kwargs: Any):
+                super().__init__(**kwargs)
+                self.number_of_players = 2
+
+        class BattleMock(Mock):
+            def __init(self, **kwargs: Any):
+                super().__init__(**kwargs)
+                self.report = random_number
+
+            def get_report(self):
+                return self.report
+
+        a = Sandbox(GameMock(), None)
+        a.battle = BattleMock()
+        report = a.get_report()
+        self.assertEqual(report, random_number)
