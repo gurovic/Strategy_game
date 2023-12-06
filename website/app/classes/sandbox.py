76751de45c6e5546161491f5d7d3d67ace68f51c
boriskiva <boriskiva3@gmail.com>
2023-12-06 12:59:39 +0300

    fixing Sandbox to match the Battle theory

--- website/app/classes/sandbox.py
+++ website/app/classes/sandbox.py
@@ -3,17 +3,19 @@ from app.models import PlayersInBattle
 
 
 class Sandbox:
-    def __init__(self, game, strategy):
+    def __init__(self, game, strategy, creator):
         self.game = game
         self.strategy = strategy
+        self.creator = creator
         players = [PlayersInBattle(player=strategy, strategy_id=0)]
         for i in range(self.game.number_of_players - 1):
             players.append(PlayersInBattle(player=self.game.ideal_solution, strategy_id=i+1))
         self.battle = Battle(self.game, players)
+        self.report = None
 
     def run_battle(self):
         self.battle.run()
 
-    def get_report(self):
-        self.report = self.battle.get_report()
-        return self.report
+    def notify(self, report):
+        self.report = report
+        self.creator(self.report)
