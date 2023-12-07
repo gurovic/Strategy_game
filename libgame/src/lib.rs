3c2b3c37a68a0bc2c345cb1b2979973d0eef2a97
MrNereof <mrnereof@gmail.com>
2023-12-07 09:42:46 +0300

    Вынес тесты из файлов

--- libgame/src/lib.rs
+++ libgame/src/lib.rs
@@ -20,11 +20,4 @@ pub fn get_battle() -> Battle<Stdin, Stdout> {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn get_battle_test() {
-        assert_eq!(get_battle_from_cli(Cli::parse_from(["test", "5"])).num_players, 5);
-    }
-}
+mod tests;
