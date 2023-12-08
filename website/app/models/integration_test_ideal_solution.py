d228fa8f5c72eb6c35ce82362e663d53b137c9e3
Ilgizar <instasamka@gmail.com>
2023-12-08 20:09:29 +0300

    IT: Ideal solution

--- website/app/models/integration_test_ideal_solution.py
+++ website/app/models/integration_test_ideal_solution.py
@@ -1,18 +1,31 @@
 from django.test import TestCase
+from .file_loader import FileLoader
+from .file import File
+from .battle import Battle
+from .game import Game
+from .players_in_battle import PlayersInBattle
 
 
 class IntegrationTest(TestCase):
-    def test_upload(self, file):
+    def test_upload(self):
+        file = File()
         report = FileLoader(file)
-        TestCase.assertEqual(report.compiler_report.status, "OK", "Failed")
+        TestCase.assertEqual(report.compiler_report.status, "OK", "Failed upload")
 
-    def test_creating_battle(self, game, players, creator):
-        battle = Battle(game, players, creator)
+    def test_creating_battle(self):
+        game = Game()
+        players = PlayersInBattle()
+        battle = Battle(game, players)
+        TestCase.assertEqual(battle.game, game, "Failed creating: different game types")
+        TestCase.assertEqual(battle.players, players, "Failed: different players' lists")
 
-    def test_run(self, game, players, creator):
-        battle = Battle(game, players, creator)
+    def test_run(self):
+        game = Game()
+        players = PlayersInBattle()
+        battle = Battle(game, players)
         battle.run()
-        TestCase.assertEqual(battle.report, "OK", "Failed")
+        TestCase.assertEqual(battle.report, "OK", "Failed running")
+
     def test_create_response(self):
         pass
 
