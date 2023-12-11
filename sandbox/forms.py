fb48e11ec358361233eab19b7ba193d13964739f
Marike <Mariabessolova23836@gmail.com>
2023-12-11 13:51:48 +0300

    tests added

--- sandbox/forms.py
+++ sandbox/forms.py
@@ -7,10 +7,12 @@ from Strategy_game.sandbox.models.sandbox import Sandbox
 class SandboxForm(forms.ModelForm):
     class Meta:
         model = SandboxForm
-    CHOICES = []
-    for x in Games:
-        CHOICES.append(x.name)
-    num_game = forms.ChoiceField(label="Game", choices=CHOICES)
-    user_strategy = forms.FileField(label="Your strategy")
-    sandbox = Sandbox(num_game, user_strategy)
-    sandbox.run_battle()
+    def __init__(self):
+        self.CHOICES = []
+        for x in Games:
+            self.CHOICES.append(x.name)
+        self.num_game = forms.ChoiceField(label="Game", choices=self.CHOICES)
+        self.user_strategy = forms.FileField(label="Your strategy")
+    def run_sandbox(self):
+        sandbox = Sandbox(self.num_game, self.user_strategy) #создали sandbox по параметрам, которые получили
+        sandbox.run_battle()
--- sandbox/tests.py
+++ sandbox/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from sandbox.forms import SandboxForm
+from database import Games #не знаю, где они хранятся
+from unittest.mock import Mock
+
+from django.test import TestCase
+
+
+class SandboxFormTest(TestCase):
+    def test_create(self):
+        a = SandboxForm()
+        allGames = []
+        for x in Games:
+            allGames.append(x.name)
+        self.asserequal(allGames, SandboxForm)
+
+    def test_run_sandbox(self):
+        pass
 
 # Create your tests here.
