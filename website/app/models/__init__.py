807aa635d61225304bc6bff7c9cb4683401649ad
boriskiva <boriskiva3@gmail.com>
2023-12-12 17:41:50 +0300

    creating class game

--- website/app/models/__init__.py
+++ website/app/models/__init__.py
@@ -1,4 +1,3 @@
-
 from app.models.compiler_report import CompilerReport
-from .model_tournament import Tournament
-
+from app.models.model_tournament import Tournament
+from app.models.game import Game
---
+++ website/app/models/game.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=50, default='')
+    number_of_players = models.IntegerField(default=0, verbose_name="Number of players")
+    ideal_solution = models.FileField(upload_to=name, verbose_name='Ideal strategy')
+    play = models.FileField(upload_to=name, verbose_name='Play')
+    win_point = models.IntegerField(default=0, null=True, blank=True, verbose_name='Point for winner')
+    lose_point = models.IntegerField(default=0, null=True, blank=True, verbose_name='Point for loser')
+    visualiser = models.FileField(upload_to=name, null=True, blank=True, verbose_name='Visualiser')
+    rules = models.FileField(upload_to=name, null=True, blank=True, verbose_name='Rules')
+
+
