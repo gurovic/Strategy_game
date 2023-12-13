96fee07fd048e55eb275089b0c0b58f700faf47b
boriskiva <boriskiva3@gmail.com>
2023-12-13 12:45:31 +0300

    creating class game

--- website/app/models/__init__.py
+++ website/app/models/__init__.py
@@ -2,4 +2,4 @@ from app.models.compiler_report import CompilerReport
 from app.models.model_tournament import Tournament
 from app.models.player_in_tournament import PlayerInTournament
 from app.models.model_battle import Battle, PlayersInBattles
-
+from app.models.game import Game
