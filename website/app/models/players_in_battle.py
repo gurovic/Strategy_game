2e8fdfbb855ae60eafb9bfd3a3755e93847a2b23
boriskiva <boriskiva3@gmail.com>
2023-12-18 15:33:51 +0300

    creating class tournament

--- website/app/models/players_in_battle.py
+++ website/app/models/players_in_battle.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class PlayersInBattle(models.Model):
-    user = models.ForeignKey('app.User', on_delete=models.CASCADE, verbose_name='Игрок')
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, verbose_name='Игрок')
     number = models.IntegerField(default=0, unique=True, verbose_name='Номер в батле')
---
+++ website/app/models/players_in_battle_test.py
@@ -0,0 +1,19 @@
+from django.db import IntegrityError
+from django.test import TestCase
+from app.models.players_in_battle import PlayersInBattle
+
+
+class PlayersInBattleTest(TestCase):
+    def setUp(self):
+        PlayersInBattle.objects.create(user=None, number=1)
+        PlayersInBattle.objects.create(user=None, number=2)
+        PlayersInBattle.objects.create(user=None, number=3)
+
+    def test_number(self):
+        player = PlayersInBattle.objects.create(user=None, number=4)
+        self.assertEqual(player.number, 4)
+
+    def test_unique_api_id_is_enforced(self):
+        """ Test that two movies with same api_id are not allowed."""
+        with self.assertRaises(IntegrityError):
+            PlayersInBattle.objects.create(number_of_points=60, place=0, id=1)
