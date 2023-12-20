0f241dde3a3671c758a372651d979155abf73795
boriskiva <boriskiva3@gmail.com>
2023-12-20 13:22:45 +0300

    creating class playersInBattle

--- website/app/models/players_in_battle.py
+++ website/app/models/players_in_battle.py
@@ -4,6 +4,7 @@ from app.models import Battle
 
 
 class PlayersInBattle(models.Model):
+    player = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
     number = models.IntegerField(default=0, unique=True, verbose_name='Номер в батле')
     battle = models.ForeignKey(Battle, null=True, default=None, on_delete=models.CASCADE, verbose_name='Battle')
     number_of_points = models.IntegerField(default=0, verbose_name='number of point')
