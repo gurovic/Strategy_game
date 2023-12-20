73326eda00ac704ca32b1d6fbe6a2c74d723a1e4
boriskiva <boriskiva3@gmail.com>
2023-12-20 12:47:27 +0300

    creating class playersInBattle

--- website/app/models/players_in_battle.py
+++ website/app/models/players_in_battle.py
@@ -7,3 +7,6 @@ class PlayersInBattle(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, verbose_name='Игрок')
     number = models.IntegerField(default=0, unique=True, verbose_name='Номер в батле')
     battle = models.ForeignKey(Battle, null=True, default=None, on_delete=models.CASCADE, verbose_name='Battle')
+    number_of_points = models.IntegerField(default=0, verbose_name='number of point')
+    place = models.IntegerField(default=0, verbose_name='place')
+    file_solution = models.FileField(upload_to='player_in_battle_files', null=True, verbose_name='file')
