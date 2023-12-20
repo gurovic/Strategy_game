6ba71778b9f7b7a476f98ea84ad523e9daac523d
boriskiva <boriskiva3@gmail.com>
2023-12-20 08:53:07 +0300

    creating class playersInBattle

--- website/app/models/players_in_battle.py
+++ website/app/models/players_in_battle.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from app.models import Battle
 
 
 class PlayersInBattle(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, verbose_name='Игрок')
     number = models.IntegerField(default=0, unique=True, verbose_name='Номер в батле')
+    battle = models.ForeignKey(Battle, null=True, default=None, on_delete=models.CASCADE, verbose_name='Battle')
