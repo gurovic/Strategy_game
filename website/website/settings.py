9e0abdf864f7f331938d99a0bc83a614e6c0eb8d
Гуровиц Владимир Михайлович <vladimir.gurovic@letovo.school>
2024-01-02 15:08:30 +0300

    changes to run django tests

--- website/website/settings.py
+++ website/website/settings.py
@@ -32,16 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.MyAppConfig',
+    'invoker.apps.InvokerConfig',
 
-    'app',
-    'invoker',
 
 ]
 
