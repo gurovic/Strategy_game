f658a338e6601b3d63ad431b6b4c0494a9aa8a40
Владимир Гуровиц <gurovic@gmail.com>
2024-01-03 12:28:41 +0300

    import paths fixed

--- website/website/settings.py
+++ website/website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,7 +142,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(PROJECT_ROOT, 'website'))
 
-
+print(PROJECT_ROOT)
 MAX_INVOKERS_COUNT = 16
 USE_DOCKER = False
