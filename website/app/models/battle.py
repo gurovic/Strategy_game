7eac696beb0adf9551588bb61b09a043afb22342
boriskiva <boriskiva3@gmail.com>
2024-01-03 19:57:04 +0300

    уборка `FileLoader`

--- website/app/models/battle.py
+++ website/app/models/battle.py
@@ -6,7 +6,7 @@ from .game import Game
 class Battle(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE, null=True)
     start_time = models.DateTimeField(auto_now_add=True)
-    players = models.ManyToManyField(User, through='PlayersInBattles')
+    players = models.ManyToManyField(User, through='PlayersInBattle')
     status = models.CharField(max_length=1, choices=[("O", "OK"), ("E", "Error"), ("T", "Time Limit"), ("N", "Not started")], default="N")  # by the rules or by errors
     total_time = models.TimeField()
     logs = models.FileField()
--- website/app/views/sandbox_views.py
+++ website/app/views/sandbox_views.py
@@ -8,22 +8,11 @@ from ..compiler import Compiler
 
 def show(request, id):
     if request.method == 'POST':
+        print(1)
         file_compiler = CompilerNotifyReceiver(request.FILES['strategy'], request.POST['language'])
+        print(2)
         file_compiler.run()
-        strategy = None
-        while strategy is None:
-            time.sleep(0.1)
-            strategy = file_compiler.compiler_report
-
-        if strategy.status == 0:
-            game = Game.objects.get(pk=id)
-            sandbox = SandboxNotifyReceiver(game, strategy)
-            report = sandbox.report()
-            while report is None:
-                time.sleep(0.1)
-                report = sandbox.report()
-            return render(request, 'sandbox.html', {'report': report})
-        else:
-            return render(request, 'sandbox.html', {'failed_report': strategy})
+        print(3)
+        return {'status': 'subscribe to compiler', 'compiler': file_compiler}
     else:
         return render(request, "sandbox.html", {})
