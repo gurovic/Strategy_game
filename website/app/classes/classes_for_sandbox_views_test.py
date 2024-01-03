1c7d8ab4c2f773cfac8721b45f9123fe337f9609
boriskiva <boriskiva3@gmail.com>
2024-01-03 17:20:10 +0300

    уборка `FileLoader`

--- website/app/classes/classes_for_sandbox_views_test.py
+++ website/app/classes/classes_for_sandbox_views_test.py
@@ -2,13 +2,44 @@ import time
 
 from django.test import TestCase, Client
 from django.urls import reverse
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, MagicMock
 
 from .classes_for_sanbox_views import CompilerNotifyReceiver, SandboxNotifyReceiver
 
 
-class TestCreateSandbox(TestCase):
-    pass
+class TestSandboxNotifyReceiver(TestCase):
+    def test_create(self):
+        class MockGame:
+            number_of_players = 0
+
+        a = SandboxNotifyReceiver(MockGame, None)
+        self.assertEqual(a.report, None)
+        self.assertEqual(a.game, MockGame)
+        self.assertEqual(a.strategy, None)
+
+    def test_run(self):
+        class MockGame:
+            number_of_players = 0
+
+        class MockSandbox:
+            count = 0
+
+            def run_battle(self):
+                self.count += 1
+
+        a = SandboxNotifyReceiver(MockGame, None)
+        a.sandbox = MockSandbox()
+        a.run()
+        self.assertEqual(a.sandbox.count, 1)
+
+    def test_notify(self):
+        class MockGame:
+            number_of_players = 0
+
+        report = 'done'
+        a = SandboxNotifyReceiver(MockGame, None)
+        a.notify(report)
+        self.assertEqual(a.report, report)
 
 
 class TestCompilerNotifyReceiver(TestCase):
@@ -16,15 +47,26 @@ class TestCompilerNotifyReceiver(TestCase):
         pass
 
     def test_lang(self):
-        langs = ['c++', 'java', 'python', 'javascript', 'c#', 'c']
-        res = ['cpp', 'Java', 'py', 'js', 'cs', 'c']
+        langs = ['c++', 'python']
+        res = ['cpp', 'py']
         for i in range(len(langs)):
             file = CompilerNotifyReceiver(None, langs[i])
             self.assertEqual(file.lang, res[i])
 
-    @patch('app.views.sandbox_views.CompileFile.compiler')
     def test_run(self):
-        pass
+        class MockCompiler:
+            count = 0
+
+            def compile(self):
+                self.count += 1
+
+        a = CompilerNotifyReceiver(None, 'c++')
+        a.compiler = MockCompiler()
+        a.run()
+        self.assertEqual(a.compiler.count, 1)
 
     def test_notify(self):
-        pass
+        report = 'done'
+        a = CompilerNotifyReceiver(None, 'c++')
+        a.notify(report)
+        self.assertEqual(a.report, report)
