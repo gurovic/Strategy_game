8339ae840bd181707d09cf9df05a59d1b8f5090c
boriskiva <boriskiva3@gmail.com>
2024-01-03 13:37:27 +0300

    уборка `FileLoader`

--- website/app/views/sandbox_views.py
+++ website/app/views/sandbox_views.py
@@ -2,31 +2,55 @@ import time
 
 from ..models.game import Game
 from ..models import CompilerReport
-from invoker.filesystem import write_file
-from ..models.sandbox import Sandbox
-from invoker.file_loader import FileLoader
+from ..classes.sandbox import Sandbox
+from ..compiler import Compiler
 
 from django.shortcuts import render
 
 
 class CompileFile:
-    def __init__(self, file):
+    LANGUAGE = {
+        'c++': 'cpp',
+        'c#': 'cs',
+        'c': 'c',
+        'python': 'py',
+        'javascript': 'js',
+        'java': 'Java',
+    }
+
+    def __init__(self, file, lang):
         self.report = None
         self.compiler_report = None
         self.file = file
+        self.lang = self.LANGUAGE[lang]
 
     def run(self):
-        path = write_file(self.file)
-        file_loader = FileLoader(path, self.notify)
+        compiler = Compiler(self.file, self.lang, self.notify)
+        compiler.compile()
 
     def notify(self, report):
         self.report = report
         self.compiler_report = CompilerReport.objects.get(pk=self.report)
 
 
+class CreateSandbox:
+    def __init__(self, game, strategy):
+        self.report = None
+        self.game = game
+        self.strategy = strategy
+        self.sandbox = Sandbox(game, strategy, self.notify)
+
+    def run(self):
+        self.sandbox.run_battle()
+
+    def notify(self, report):
+        self.report = report
+
+
 def show(request, id):
     if request.method == 'POST':
-        file_compiler = CompileFile(request.FILES['strategy'])
+        print(request.POST['language'])
+        file_compiler = CompileFile(request.FILES['strategy'], request.POST['language'])
         file_compiler.run()
         strategy = None
         while strategy is None:
@@ -35,8 +59,11 @@ def show(request, id):
 
         if strategy.status == 0:
             game = Game.objects.get(pk=id)
-            sandbox = Sandbox(game, strategy)
-            report = sandbox.get_report()
+            sandbox = CreateSandbox(game, strategy)
+            report = sandbox.report()
+            while report is None:
+                time.sleep(0.1)
+                report = sandbox.report()
             return render(request, 'sandbox.html', {'report': report})
         else:
             return render(request, 'sandbox.html', {'failed_report': strategy})
