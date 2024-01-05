a1c37949f03cde41f92c00a9b28a3cd08108b2cd
boriskiva <boriskiva3@gmail.com>
2024-01-05 16:27:14 +0300

    уборка `FileLoader`

--- website/app/views/sandbox_views.py
+++ website/app/views/sandbox_views.py
@@ -6,6 +6,8 @@ from ..models import CompilerReport, Game
 from ..classes import Sandbox, SandboxNotifyReceiver, CompilerNotifyReceiver
 from ..compiler import Compiler
 
+TYPE = 'test'
+
 
 def show(request, game_id):
     game = Game.objects.get(pk=game_id)
@@ -14,12 +16,16 @@ def show(request, game_id):
             file_object = request.FILES['strategy']
             lang = request.POST['language']
             file_compiler = CompilerNotifyReceiver(file_object, lang)
-            # file_compiler.run()
-            report = CompilerReport.objects.create(
-                compiled_file=file_object,
-                status=CompilerReport.Status.OK,
-            )
-            file_compiler.notify(report)
+
+            if TYPE == 'test':
+                report = CompilerReport.objects.create(
+                    compiled_file=file_object,
+                    status=CompilerReport.Status.OK,
+                )
+                file_compiler.notify(report)
+            else:
+                file_compiler.run()
+
             return render(request, 'sandbox.html',
                           {'status': 'receive compiler report', 'compiler_report': file_compiler.report, 'game': game})
         elif request.POST['type'] == 'sandbox':
@@ -27,8 +33,16 @@ def show(request, game_id):
             compiler_report = CompilerReport.objects.get(pk=compiler_report_id)
             game = Game.objects.get(pk=game_id)
             sandbox = SandboxNotifyReceiver(game, compiler_report.compiled_file)
-            return render(request, 'sandbox.html', {'status': 'subscribe to sandbox', 'game': game})
+
+            if TYPE == 'test':
+                report = {}
+                sandbox.notify(report)
+            else:
+                sandbox.run()
+
+            return render(request, 'sandbox.html',
+                          {'status': 'subscribe to sandbox', 'game': game, 'sandbox_report': sandbox.report})
         else:
             return render(request, 'sandbox.html', {'status': 'failed', 'game': game})
     else:
-        return render(request, "sandbox.html", {'game': game})
+        return render(request, "sandbox.html", {'status': 'filling compilation form', 'game': game})
