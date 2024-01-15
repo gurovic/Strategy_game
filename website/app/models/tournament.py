39dbd29af3ed8cd9b9bd769c06689515a06f3ae1
boriskiva <boriskiva3@gmail.com>
2024-01-15 15:37:43 +0300

    fixing tests with tournament

--- website/app/models/tournament.py
+++ website/app/models/tournament.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
+from datetime import datetime, timezone
 
 from .battle import Battle
 from .game import Game
@@ -18,8 +18,8 @@ class Tournament(models.Model):
     name = models.CharField(max_length=255, default='tournament', verbose_name='Name')
     game = models.ForeignKey(Game, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Game')
     players = models.ManyToManyField(User, through='PlayerInTournament', null=True, blank=True, verbose_name='Players')
-    system = models.ForeignKey(TournamentSystem, on_delete=models.CASCADE, null=False, blank=False, verbose_name='Tournament System')
-    start_time = models.DateTimeField(null=True, verbose_name='Start Time')
+    system = models.ForeignKey(TournamentSystem, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Tournament System')
+    start_time = models.DateTimeField(blank=True, null=True, verbose_name='Start Time')
     end_time = models.DateTimeField(null=True, verbose_name='Finish time')
     status = models.IntegerField(choices=Status.choices, default=Status.NOT_STARTED, verbose_name='Status')
     battles = models.ManyToManyField(Battle, blank=True, verbose_name='Battle')
