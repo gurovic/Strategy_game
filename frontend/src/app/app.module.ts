5a25ba9134232d0652c310f0de2cb1528b5e1651
boriskiva <boriskiva3@gmail.com>
2024-01-18 13:14:13 +0300

    это база

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
