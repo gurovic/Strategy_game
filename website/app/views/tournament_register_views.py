d2f129591d63af45cd7e6b565d11fc95bebc88ee
Marike <Mariabessolova23836@gmail.com>
2024-01-18 16:54:06 +0300

    bhjbhjbbhbhjbbjbhvjvjhvjvjv

--- website/app/views/tournament_register_views.py
+++ website/app/views/tournament_register_views.py
@@ -8,12 +8,14 @@ def register(request, tournament_id, user_id):
 
         if len(ourtournament.players) != ourtournament.max_of_players: #если можем зарегистрировать
             ouruser = User.objects.filter(pk=user_id).first
-            ourplayer = PlayerInTournament()
+            ourplayer = PlayerInTournament.objects.create()
             ourplayer.player = ouruser
             ourplayer.tournament = ourtournament
-            return render("registered_intournament.html")
+            ourplayer.file_solution = 0
+            ourplayer.save()
+            return render("registered_intournament.html", {'status': 'registered'})
         else:
-            return 0
+            return render("registered_intournament.html", {'status': 'denied registration'})
 
     else:
-        return render(request, "register_intournament.html")
+        return render(request, "register_intournament.html", {'status': 'not registered'})
