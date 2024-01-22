21aecef602e9aca1e7ab7aad08d3ba9baa45343a
boriskiva <boriskiva3@gmail.com>
2024-01-22 21:22:25 +0300

    some changes doing main page mainly

--- frontend/src/app/components/main-page/main-page.component.ts
+++ frontend/src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, AfterViewInit, HostListener } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
@@ -6,14 +6,46 @@ import {ActivatedRoute, Router} from "@angular/router";
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements AfterViewInit {
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
   ) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    let vertical_01_lines = document.getElementsByClassName('vertical-line')!;
+    for (let i = 0; i < vertical_01_lines.length; i++) {
+      // @ts-ignore
+      vertical_01_lines[i].innerText=this.generate_01_text_line();
+    }
+    console.log(1);
+
+    addEventListener("scroll", (event)=>{
+      console.log(1);
+      for (let i = 0; i < vertical_01_lines.length; i++) {
+        let element = vertical_01_lines[i];
+        let y = window.scrollY;
+        console.log(y);
+        // @ts-ignore
+        element.style.top = `${y*2}px`
+      }
+    })
   }
 
+
+
+  getRandomInt(max:number): number {
+    return Math.floor(Math.random() * max);
+  }
+
+  generate_01_text_line() {
+    let result = "";
+    for (let i = 0; i < 200; i++) {
+      if (this.getRandomInt(2) == 0) result+="0";
+      else result+="1";
+    }
+    // this.cdr.detectChanges();
+    return result;
+  }
 }
--- frontend/src/app/components/top-bar/top-bar.component.ts
+++ frontend/src/app/components/top-bar/top-bar.component.ts
@@ -2,65 +2,77 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-    selector: 'app-top-bar',
-    templateUrl: './top-bar.component.html',
-    styleUrls: ['./top-bar.component.scss']
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
 
-    constructor(
-        private router: Router,
-        private route: ActivatedRoute,
-    ) {
-    }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
-    go_link(link: string): void {
-        this.close_menu();
-        this.router.navigate([link]);
-        window.scrollTo({
-            // @ts-ignore
-            top: 0,
-            behavior: "smooth"
-        });
-    }
+  go_link(link: string): void {
+    this.close_menu();
+    this.router.navigate([link]);
+    window.scrollTo({
+      // @ts-ignore
+      top: 0,
+      behavior: "smooth"
+    });
+  }
 
-    open_menu() {
-        let menu = document.getElementById('dropdown-menu')!;
-        let rects = document.getElementsByClassName('menu-svg-rect')!;
-        // @ts-ignore
-        rects[0].style.transform = 'rotate(-45deg) translateY(30px)'
-        // @ts-ignore
-        rects[1].style.transform = 'translateX(100px)'
-        // @ts-ignore
-        rects[2].style.transform = 'rotate(45deg) translateY(-30px)'
-        menu.style.display = 'block';
-        setTimeout(() => {
-            menu.style.transform = 'translateY(0)';
-        })
-    }
+  open_menu() {
+    let menu = document.getElementById('dropdown-menu')!;
+    let rects = document.getElementsByClassName('menu-svg-rect')!;
+    // @ts-ignore
+    rects[0].style.transform = 'rotate(-180deg) translateY(30px)'
+    // @ts-ignore
+    rects[0].style.opacity = '0';
+    // @ts-ignore
+    rects[1].style.opacity = '.8';
+    // @ts-ignore
+    rects[1].style.transform = 'rotate(180deg)'
+    // @ts-ignore
+    rects[2].style.transform = 'rotate(-180deg) translateY(-30px)'
+    // @ts-ignore
+    rects[2].style.opacity = '0';
+    menu.style.display = 'block';
+    setTimeout(() => {
+      menu.style.transform = 'translateY(0)';
+    })
+  }
 
-    close_menu() {
-        let menu = document.getElementById('dropdown-menu')!;
-        let rects = document.getElementsByClassName('menu-svg-rect')!;
-        // @ts-ignore
-        rects[0].style.transform = 'rotate(0deg) translateY(0px)'
-        // @ts-ignore
-        rects[1].style.transform = 'translateX(0px)'
-        // @ts-ignore
-        rects[2].style.transform = 'rotate(0deg) translateY(0px)'
-        menu.style.transform = 'translateY(-100%)';
-        setTimeout(() => {
-            menu.style.display = 'none';
-        }, 500)
-    }
+  close_menu() {
+    let menu = document.getElementById('dropdown-menu')!;
+    let rects = document.getElementsByClassName('menu-svg-rect')!;
+    // @ts-ignore
+    rects[0].style.transform = 'rotate(0deg) translateY(0px)'
+    // @ts-ignore
+    rects[0].style.opacity = '1';
+    // @ts-ignore
+    rects[1].style.opacity = '1';
+    // @ts-ignore
+    rects[1].style.transform = 'translateX(0px)'
+    // @ts-ignore
+    rects[2].style.transform = 'rotate(0deg) translateY(0px)'
+    // @ts-ignore
+    rects[2].style.opacity = '1';
+    menu.style.transform = 'translateY(-100%)';
+    setTimeout(() => {
+      menu.style.display = 'none';
+    }, 500)
+  }
 
-    interact_menu() {
-        let menu = document.getElementById('dropdown-menu')!;
+  interact_menu() {
+    let menu = document.getElementById('dropdown-menu')!;
 
-        if (menu.style.display == 'none' || menu.style.display == '') this.open_menu();
-        else this.close_menu();
-    }
+    if (menu.style.display == 'none' || menu.style.display == '') this.open_menu();
+    else this.close_menu();
+  }
 }
