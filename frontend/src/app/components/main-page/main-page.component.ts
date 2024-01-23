ade5b403a04d95e3aba1be86fed16c3b35528ee8
boriskiva <boriskiva3@gmail.com>
2024-01-23 11:08:05 +0300

    Я молодец что то делаю, а что разбирайтесь сами

--- frontend/src/app/components/main-page/main-page.component.ts
+++ frontend/src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, AfterViewInit, HostListener } from '@angular/core';
+import {Component, AfterViewInit, HostListener} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
@@ -23,28 +23,11 @@ export class MainPageComponent implements AfterViewInit {
   constructor(
     public router: Router,
     private route: ActivatedRoute,
-  ) { }
+  ) {
+  }
 
   ngAfterViewInit(): void {
-    let vertical_01_lines = document.getElementsByClassName('vertical-line')!;
-    for (let i = 0; i < vertical_01_lines.length; i++) {
-      let line_content = this.generate_01_text_line();
-      console.log(line_content);
-      // @ts-ignore
-      vertical_01_lines[i].innerText = line_content;
-    }
-
-    addEventListener("scroll", (event)=>{
-      console.log(1);
-      for (let i = 0; i < vertical_01_lines.length; i++) {
-        let element = vertical_01_lines[i];
-        let y = window.scrollY;
-        console.log(y);
-        // @ts-ignore
-        element.style.top = `${y*2}px`
-      }
-    })
-
+    ///////////////////// observers ///// /////////
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         let class_name = 'show';
@@ -72,17 +55,21 @@ export class MainPageComponent implements AfterViewInit {
     hiddenCards.forEach((el) => cards_observer.observe(el));
   }
 
-  getRandomInt(max:number): number {
+  getRandomInt(max: number): number {
     return Math.floor(Math.random() * max);
   }
 
   generate_01_text_line() {
     let result = "";
     for (let i = 0; i < 200; i++) {
-      if (this.getRandomInt(2) == 0) result+="0";
-      else result+="1";
+      if (this.getRandomInt(2) == 0) result += "0";
+      else result += "1";
     }
     // this.cdr.detectChanges();
     return result;
+  }
+
+  go(link: string) {
+    this.router.navigate([link]);
   }
 }
