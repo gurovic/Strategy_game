cee464d0f87a1032eecc6f4b5107c944d92333f0
boriskiva <boriskiva3@gmail.com>
2024-01-23 20:35:41 +0300

    Я молодец что то делаю, а что разбирайтесь сами

---
+++ frontend/src/app/components/addons/archive-task/archive-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-archive-task',
+  templateUrl: './archive-task.component.html',
+  styleUrls: ['./archive-task.component.scss']
+})
+export class ArchiveTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
