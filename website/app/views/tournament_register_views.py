f3ff689e82ab462b0057e7d75178bc168981111c
Marike <Mariabessolova23836@gmail.com>
2024-01-24 11:12:39 +0300

    final commit2

--- website/app/views/tournament_register_views.py
+++ website/app/views/tournament_register_views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 def register(request, tournament_id, user_id):
     if request.method == 'POST':
-        ourtournament = Tournament.objects.filter(pk=tournament_id).first()
+        ourtournament = Tournament.objects.get(pk=tournament_id).first()
         try:
             try_to_get_player = ourtournament.players.get(pk=user_id)
         except:
@@ -14,8 +14,8 @@ def register(request, tournament_id, user_id):
 
         if try_to_get_player is None and len(
                 ourtournament.players.all()) != ourtournament.max_of_players:  # если можем зарегистрировать
-            ouruser = User.objects.filter(pk=user_id).first()
-            ourplayer = PlayerInTournament.objects.create(player=ouruser, tournament=ourtournament, file_solution=None)
+            ouruser = User.objects.get(pk=user_id).first()
+            #ourplayer = PlayerInTournament.objects.create(player=ouruser, tournament=ourtournament, file_solution=None)
             ourtournament.players.add(ouruser)
             return render(request, 'register_intournament.html', {'status': 'registered'})
         elif try_to_get_player != None:
