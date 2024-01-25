07058a93fdbb54e0191829f02958470c50288596
boriskiva <boriskiva3@gmail.com>
2024-01-25 21:54:37 +0300

    about us page

--- frontend/src/app/services/our-team.service.ts
+++ frontend/src/app/services/our-team.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 export interface Member {
   name: string;
   image_path: string;
-
+  position: string;
 }
 
 @Injectable({
@@ -11,13 +11,17 @@ export interface Member {
 })
 export class OurTeamService {
   public members: Member[] = [
-    {name: 'Vladimir Gurovic', image_path: 'assets/our_team_photos/boris.png' },
-    {name: 'Boris Kiva', image_path: 'assets/our_team_photos/boris.png' },
-    {name: 'Vladimir Shaklein', image_path: 'assets/our_team_photos/boris.png' },
-    {name: 'Seraphim Lobanov', image_path: 'assets/our_team_photos/boris.png' },
-    {name: 'Michel Countryside', image_path: 'assets/our_team_photos/boris.png' },
-    {name: 'Michel Lichmanov', image_path: 'assets/our_team_photos/boris.png' },
-    {name: 'Maria Bessolova', image_path: 'assets/our_team_photos/boris.png' },
+    {name: 'Vladimir Gurovic', image_path: 'assets/our_team_photos/boris.png', position: 'Team Lead' },
+    {name: 'Boris Kiva', image_path: 'assets/our_team_photos/boris.png', position: 'FullStack dev' },
+    {name: 'Vladimir Shaklein', image_path: 'assets/our_team_photos/boris.png', position: 'Backend dev' },
+    {name: 'Seraphim Lobanov', image_path: 'assets/our_team_photos/boris.png', position: 'FullStack dev' },
+    {name: 'Maria Bessolova', image_path: 'assets/our_team_photos/boris.png', position: 'Backend dev' },
+    {name: 'Ilgizar Khazeev', image_path: 'assets/our_team_photos/boris.png', position: 'Thinker' },
+    {name: 'Michel Countryside', image_path: 'assets/our_team_photos/boris.png', position: 'GPT-user' },
+    {name: 'Michel Lichmanov', image_path: 'assets/our_team_photos/boris.png', position: 'still doing math' },
+    {name: 'Dmitry Dubrov', image_path: 'assets/our_team_photos/boris.png', position: 'None' },
+    {name: 'Dmitry Polov', image_path: 'assets/our_team_photos/boris.png', position: 'User haha' },
+    {name: 'Alexandra ...', image_path: 'assets/our_team_photos/boris.png', position: 'Thinker' },
   ]
 
   constructor() { }
