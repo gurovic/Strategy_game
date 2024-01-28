b0698c09f1df2e0dc1e0c78cb7e75a6967c482b4
boriskiva <boriskiva3@gmail.com>
2024-01-29 00:03:36 +0300

    making amazing running stroke

--- frontend/src/app/components/__PAGES/contests/contests.component.ts
+++ frontend/src/app/components/__PAGES/contests/contests.component.ts
@@ -23,21 +23,47 @@ export class ContestsComponent implements OnInit {
         return result;
     }
 
+    change_text_forward(id:number, count:number = 100) {
+        let text = this.cards[id].about_text;
+        let card_text = document.getElementById('card-text-'+id)!;
+        if (count >= text.length) {
+            card_text.innerText = text;
+            this.opened = id;
+            return;
+        }
+
+        card_text.innerText = text.slice(0,count)+'...';
+        setTimeout(()=>{
+            this.change_text_forward(id,count+1);
+        },(text.length-100)/1000)
+    }
+    change_text_reverse(id: number, count: number) {
+        let text = this.cards[id].about_text;
+        let card_text = document.getElementById('card-text-'+id)!;
+        card_text.innerText = text.slice(0, count)+'...';
+
+        if (count<=100) {
+            this.opened=-1;
+            return;
+        }
+
+        setTimeout(()=>{
+            this.change_text_reverse(id,count-1);
+        },(text.length-100)/1000)
+    }
+
     show_more_about_text(card_index: number) {
-        console.log(this.opened, card_index);
         let card_bp_id = 'text-part-of-card-'+card_index;
         let card_id = 'card'+card_index;
         if (this.opened != -1) {
             let prev_text_part = document.getElementById('text-part-of-card-'+this.opened)!;
-            prev_text_part.style.height = 'var(--size)';
-            if (this.opened == card_index) { this.opened = -1; return; }
-            this.opened = -1;
+            this.change_text_reverse(this.opened, this.cards[card_index].about_text.length);
+            if (this.opened == card_index) return;
         }
         let text_part = document.getElementById(card_bp_id)!;
         let card = document.getElementById(card_id)!;
-        text_part.style.height = '400px';
 
-        this.opened = card_index;
+        this.change_text_forward(card_index);
     }
 
 }
