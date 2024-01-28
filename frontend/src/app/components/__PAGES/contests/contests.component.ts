db1ba037d21415f64ff1274138b53f6585ea8f62
boriskiva <boriskiva3@gmail.com>
2024-01-28 21:43:06 +0300

    some changes

--- frontend/src/app/components/__PAGES/contests/contests.component.ts
+++ frontend/src/app/components/__PAGES/contests/contests.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ContestCard, debug_cards} from "../../../interface/contest_card";
+import {text} from "d3";
 
 @Component({
     selector: 'app-contests',
@@ -8,11 +9,35 @@ import {ContestCard, debug_cards} from "../../../interface/contest_card";
 })
 export class ContestsComponent implements OnInit {
     public cards = debug_cards;
+    public opened = -1;
 
     constructor() {
     }
 
     ngOnInit(): void {
+    }
+
+    get_text(text:string): string {
+        let result:string = text.slice(0,100) + '...';
+
+        return result;
+    }
+
+    show_more_about_text(card_index: number) {
+        console.log(this.opened, card_index);
+        let card_bp_id = 'text-part-of-card-'+card_index;
+        let card_id = 'card'+card_index;
+        if (this.opened != -1) {
+            let prev_text_part = document.getElementById('text-part-of-card-'+this.opened)!;
+            prev_text_part.style.height = 'var(--size)';
+            if (this.opened == card_index) { this.opened = -1; return; }
+            this.opened = -1;
+        }
+        let text_part = document.getElementById(card_bp_id)!;
+        let card = document.getElementById(card_id)!;
+        text_part.style.height = '400px';
+
+        this.opened = card_index;
     }
 
 }
