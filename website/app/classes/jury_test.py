c4e6cba41c870c9c61f4f262a99025c5054a2c23
Ilgizar <instasamka@gmail.com>
2024-01-28 21:48:15 +0300

    Jury(Ilgizar)

--- website/app/classes/jury_test.py
+++ website/app/classes/jury_test.py
@@ -3,42 +3,42 @@ from unittest.mock import patch, Mock
 
 from .jury import Jury
 from ...invoker.invoker_multi_request import InvokerMultiRequest
-from ...invoker.invoker_request import InvokerRequest
-from ...invoker.invoker_process import InvokerProcess
+from ...invoker.invoker_request import InvokerRequest, InvokerRequestType
 
 
 class TestJury(TestCase):
-    @patch("invoker.invoker_multi_request.InvokerMultiRequest.invoker_requests")
-    def test_get_invoker_requests(self, mock_invoker_requests: Mock):
+    def test_get_invoker_requests(self):
         play_invoker_request = InvokerRequest("command")
         play_invoker_request.type = InvokerRequestType.PLAY
         strategy_invoker_request = InvokerRequest("command")
         strategy_invoker_request.type = InvokerRequestType.STRATEGY
-
-        mock_invoker_requests.return_value = [play_invoker_request, strategy_invoker_request]
 
         invoker_multi_request = InvokerMultiRequest([play_invoker_request, strategy_invoker_request])
 
         jury = Jury(invoker_multi_request)
 
         self.assertEqual(jury.play_invoker_request, play_invoker_request)
-        self.assertEqual(jury.strategies_invoker_requests,[strategy_invoker_request])
+        self.assertEqual(jury.strategies_invoker_requests, [strategy_invoker_request])
 
-    @patch("app.classes.jury.Jury.get_invoker_requests")
-    @patch("app.classes.jury.Jury.play_invoker_request.process_callback")
-    @patch("app.classes.jury.Jury.strategies_invoker_requests")
-    def test_get_processes(self, mock_strategies_invoker_requests: Mock, mock_play_process: Mock, mock_get_invoker_requests: Mock):
-        mock_play_process.return_value = InvokerProcess()
-        strategy_process = InvokerProcess()
+    class InvokerProcessMock:
+        def __init__(self):
+            pass
 
+    def test_get_processes(self):
+        strategy_process = self.InvokerProcessMock()
         strategy_invoker_request = InvokerRequest("command")
+        strategy_invoker_request.type = InvokerRequestType.STRATEGY
         strategy_invoker_request.process_callback = strategy_process
-        mock_strategies_invoker_requests.return_value = [strategy_invoker_request]
 
-        invoker_multi_request = InvokerMultiRequest([])
+        play_process = self.InvokerProcessMock()
+        play_invoker_request = InvokerRequest("command")
+        play_invoker_request.type = InvokerRequestType.PLAY
+        play_invoker_request.callback = play_process
+
+        invoker_multi_request = InvokerMultiRequest([strategy_invoker_request, play_invoker_request])
 
         jury = Jury(invoker_multi_request)
 
-        self.assertEqual(jury.play_process, mock_play_process)
+        self.assertEqual(jury.play_process, play_process)
         self.assertEqual(jury.strategies_process, [strategy_process])
 
