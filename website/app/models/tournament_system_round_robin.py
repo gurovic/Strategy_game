dc5a2830727bb91797f0c2ef36ebe1bbb1c9df88
boriskiva <boriskiva3@gmail.com>
2024-01-29 19:07:52 +0300

    changing tournament system roundrobin to model

--- website/app/models/tournament_system_round_robin.py
+++ website/app/models/tournament_system_round_robin.py
@@ -1,25 +1,27 @@
+from django.db import models
+
+from . import Tournament
 from .tournamentsystem import TournamentSystem
 from .battle import Battle
 
+
 class TournamentSystemRoundRobin(TournamentSystem):
-    def __init__(self, tournament):
-        super().__init__(tournament)
+    def run_tournament(self):
         self.tournament_players = dict()
         for player in self.tournament.players.all():
             self.tournament_players[player.player] = player
-        self.battle_count = len(self.tournament_players)*(len(self.tournament_players)-1)/2
+        self.battle_count = len(self.tournament_players) * (len(self.tournament_players) - 1) / 2
 
-    def run_tournament(self):
         participants = list(zip(self.tournament_players.keys(), self.tournament_players.values()))
         for i in range(len(participants)):
-            for j in range(i+1, len(participants)):
+            for j in range(i + 1, len(participants)):
                 battle = Battle(self.tournament.game, list([participants[i][1], participants[j][1]]), self)
                 self.tournament.battles.append(battle)
                 battle.start()
 
     def calculate_places(self):
         places = list(zip(self.tournament_players.keys(), self.tournament_players.values()))
-        places = sorted(places, key = lambda x: x[1].number_of_points)
+        places = sorted(places, key=lambda x: x[1].number_of_points)
         number = 1
         for place in places:
             place[1].place = number
@@ -36,5 +38,3 @@ class TournamentSystemRoundRobin(TournamentSystem):
         if self.battle_count == 0:
             self.calculate_places()
             self.finish()
-
-
