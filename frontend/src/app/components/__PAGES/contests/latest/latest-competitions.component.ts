87ffa3f58b9114bbf0167654edb1945255951f30
boriskiva <boriskiva3@gmail.com>
2024-01-30 11:04:25 +0300

    хз

---
+++ frontend/src/app/components/__PAGES/contests/latest/latest-competitions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-competitions',
+  templateUrl: './latest-competitions.component.html',
+  styleUrls: ['./latest-competitions.component.scss']
+})
+export class LatestCompetitionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
