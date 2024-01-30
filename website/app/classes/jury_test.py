46fccd8eada18d87742c58d376e850f36d9b5a16
Sasha G <a1exgo111v@gmail.com>
2024-01-30 12:05:13 +0300

    fixing test_get_invoker_requests

--- website/app/classes/jury_test.py
+++ website/app/classes/jury_test.py
@@ -11,7 +11,17 @@ from invoker.invoker_request import InvokerRequest, InvokerRequestType
 class TestJury(TestCase):
     @patch('app.classes.jury.Jury.get_processes')
     def test_get_invoker_requests(self, mock_get_processes: Mock):
-        pass
+        play_invoker_request = InvokerRequest("command")
+        play_invoker_request.type = InvokerRequestType.PLAY
+        strategy_invoker_request = InvokerRequest("command")
+        strategy_invoker_request.type = InvokerRequestType.STRATEGY
+
+        invoker_multi_request = InvokerMultiRequest([play_invoker_request, strategy_invoker_request])
+
+        jury = Jury(invoker_multi_request)
+
+        self.assertEqual(jury.play_invoker_request, play_invoker_request)
+        self.assertEqual(jury.strategies_invoker_requests, [strategy_invoker_request])
 
     def test_get_processes(self):
         pass
