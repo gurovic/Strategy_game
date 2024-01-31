3bdc011d52b8f95567c2df041375573402a8589b
MIHAIL ZAGORODNUYK <mikle.zag@gmail.com>
2024-01-31 13:26:25 +0300

    commin

--- website/app/urls.py
+++ website/app/urls.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from .views import sandbox_views, tournament_views
+from .views.game_upload_views import GameUploadView
 
 urlpatterns = [
     path('sandbox/<int:game_id>', sandbox_views.show),
     path('tournament/', tournament_views.start_page, name="tounrament_startpage"),
-    path('tournament/create/', tournament_views.create_tounament, name="create_tournament")
+    path('tournament/create/', tournament_views.create_tounament, name="create_tournament"),
+    path('game_upload/', GameUploadView.as_view(), name="game_upload")
     #path('sandbox', app.views.sandbox.run_SandboxForm, name='SandboxForm'),
     #path('sandbox/run/<int:id>', app.views.run_Sandbox),
 
