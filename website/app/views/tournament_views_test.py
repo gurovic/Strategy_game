a0107f17a217774a89aac6b1fd0a4b0af74d0fbb
Palov Dmitriy <palov.dmitriy@yandex.ru>
2024-01-31 14:48:10 +0300

    fixed test, though tournament froms still doesn't work :(

--- website/app/views/tournament_views_test.py
+++ website/app/views/tournament_views_test.py
@@ -1,24 +1,31 @@
 from django.test import TestCase
-import unittest
+from unittest.mock import Mock, patch
 from ..forms import TournamentForm
+from ..models.game import Game
+
 
 
 class TestTournamentViews(TestCase):
 
-    def test_if_form_is_valid(self, request):
-        if request.method == "POST":
-            form = TournamentForm(request.POST)
-            if (form.is_valid()):
-                response = self.client.get('/app/tournament/create')
-                self.assertEqual(response.status_code, 200)
-            else:
-                response = self.client.get('/app/tournament/create')
-                self.assertEqual(response.status_code, 200)
-                self.assertTemplateUsed(response, 'tournament_create.html')
+    def test_if_form_is_valid(self):
+        mock_game = Mock()
+        data = {'name':'example_tournament', 'game':None, 'system':0, 'start_time':'2006-10-25 14:30:59', 'end_time':'2006-10-26 14:30:59', 'max_of_players':10}
+        #if request.method == "POST":
+        form = TournamentForm(data)
+        if (form.is_valid()):
+           response = self.client.get('/app/tournament/create/')
+           self.assertEqual(response.status_code, 200)
         else:
-            response = self.client.get('/app/tournament/create')
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'tournament_create.html')
+            self.fail()
+
+        #else:
+        #    response = self.client.get('/app/tournament/create/')
+        #    self.assertEqual(response.status_code, 200)
+        #    self.assertTemplateUsed(response, 'tournament_create.html')
+        #else:
+        #    response = self.client.get('/app/tournament/create')
+        #    self.assertEqual(response.status_code, 200)
+        #    self.assertTemplateUsed(response, 'tournament_create.html')
 
 
 
@@ -30,6 +37,6 @@ class TestTournamentViews(TestCase):
 
 
     def test_correct_template_2(self):
-        response = self.client.get('/app/tournament/create')
+        response = self.client.get('/app/tournament/create/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'tournament_create.html')
