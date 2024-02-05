6fbc1e7e38b748df1844e73f047cd48a91ede13a
Владимир Гуровиц <gurovic@gmail.com>
2024-02-05 15:14:31 +0300

    filename fixed

--- website/invoker/invoker_process_test.py
+++ website/invoker/invoker_process_test.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from unittest.mock import patch, Mock
 from invoker.invoker import Invoker
 from invoker.invoker import RunResult
-from invoker.InvokerProcess import InvokerProcess
+from invoker.invoker_process import InvokerProcess
 import typing
 
 
@@ -19,15 +19,15 @@ class TestInvokerProcess(TestCase):
         def wait(self, timeout: typing.Optional[int] = None):
             pass
 
-    @patch("invoker.InvokerProcess_test.TestInvokerProcess.InvokerProcessForTesting.kill")
-    @patch("invoker.InvokerProcess_test.TestInvokerProcess.InvokerProcessForTesting.make_run_result")
-    @patch("invoker.InvokerProcess_test.TestInvokerProcess.InvokerProcessForTesting.wait")
+    @patch("invoker.invoker_process_test.TestInvokerProcess.InvokerProcessForTesting.kill")
+    @patch("invoker.invoker_process_test.TestInvokerProcess.InvokerProcessForTesting.make_run_result")
+    @patch("invoker.invoker_process_test.TestInvokerProcess.InvokerProcessForTesting.wait")
     def test_create(self, mock_kill: Mock, mock_make_run_result: Mock, mock_wait: Mock):
         invoker_process = self.InvokerProcessForTesting()
 
-    @patch("invoker.InvokerProcess_test.TestInvokerProcess.InvokerProcessForTesting.kill")
-    @patch("invoker.InvokerProcess_test.TestInvokerProcess.InvokerProcessForTesting.make_run_result")
-    @patch("invoker.InvokerProcess_test.TestInvokerProcess.InvokerProcessForTesting.wait")
+    @patch("invoker.invoker_process_test.TestInvokerProcess.InvokerProcessForTesting.kill")
+    @patch("invoker.invoker_process_test.TestInvokerProcess.InvokerProcessForTesting.make_run_result")
+    @patch("invoker.invoker_process_test.TestInvokerProcess.InvokerProcessForTesting.wait")
     def test_with_callback(self, mock_kill: Mock, mock_make_run_result: Mock, mock_wait: Mock):
         mock_callback = Mock()
         invoker_process = self.InvokerProcessForTesting(callback=mock_callback)
