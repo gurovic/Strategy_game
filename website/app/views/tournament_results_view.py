005b4db55b7ef816b8236145ff05565119c143ea
Sasha G <a1exgo111v@gmail.com>
2024-02-06 08:48:08 +0300

    create .html

--- website/app/views/tournament_results_view.py
+++ website/app/views/tournament_results_view.py
@@ -7,5 +7,4 @@ from ..models import Tournament, PlayerInTournament
 def start_page(request, tournament_id):
     tournament = Tournament.objects.get(pk=tournament_id)
     if request.method == 'POST':
-
-    return HttpResponse(f"Страница отображения результатов турнира {tournament_id}")
+        return render(request, 'tournament_results')
