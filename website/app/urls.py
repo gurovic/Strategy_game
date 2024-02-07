4772c7e8a57385a2b099f4eabdcc7555a4c217a1
MIHAIL ZAGORODNUYK <mikle.zag@gmail.com>
2024-02-07 14:12:02 +0300

    game_upload_1

--- website/app/urls.py
+++ website/app/urls.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-from .views import sandbox_views, tournament_views, tournament_start_view, tournament_register_views
+from .views import sandbox_views, tournament_views, tournament_start_view, tournament_register_views, game_upload_views
 
 urlpatterns = [
     path('sandbox/<int:game_id>', sandbox_views.show),
     path('tournament/', tournament_views.start_page, name="tounrament_startpage"),
     path('tournament/start/<int:tournament_id>', tournament_start_view.start_tournament),
     path('tournament/create/', tournament_views.create_tounament, name="create_tournament"),
-    path('tournament/register/<int:tournament_id>/<int:user_id>', tournament_register_views.register)
+    path('tournament/register/<int:tournament_id>/<int:user_id>', tournament_register_views.register),
+    path('game_upload/', game_upload_views.GameUploadView.as_view(), name='game_upload'),
 ]
 
 if settings.DEBUG:
--- website/app/views/game_upload_views.py
+++ website/app/views/game_upload_views.py
@@ -1,9 +1,19 @@
+from functools import partial
+
 from django.shortcuts import render
 from django.views import View
-from ..game_upload_form import GameForm
-from ..classes import LANGUAGES
+
 from ..compiler import Compiler
-from functools import partial
+from ..game_upload_form import GameForm
+
+LANGUAGES = {
+    'c++': 'cpp',
+    'c#': 'cs',
+    'c': 'c',
+    'python': 'py',
+    'javascript': 'js',
+    'java': 'Java',
+}
 
 
 class GameUploadView(View):
@@ -11,59 +21,62 @@ class GameUploadView(View):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.visualizer_report = None
-        self.play_status = None
+        self.visualizer = None
+        self.play = None
+        self.ideal_solution = None
         self.play_report = None
-        self.ideal_solution_status = None
-        self.visualizer_status = None
+        self.visualizer_report = None
         self.ideal_solution_report = None
         self.game_form = GameForm()
 
     def get(self, request, *args, **kwargs):
-        print(0)
         return render(request, self.template_name, {
-            'status': 'filling compilation form',
+            'status': 'compilation form',
             'game_form': self.game_form,
             'available_languages': LANGUAGES,
         })
 
     def post(self, request, *args, **kwargs):
+        print(request.POST['type'])
         if request.POST['type'] == 'compiler':
             self.game_form = GameForm(request.POST)
-            ideal_solution = Compiler(
-                request.FILES['ideal_solution'],
+            self.ideal_solution = Compiler(
+                request.FILES['ideal_solution'].read(),
                 LANGUAGES[request.POST['ideal_solution_language']],
-                partial(self.notify, 'ideal_solution')
+                partial(self.notify, label='ideal_solution')
             )
-            play = Compiler(
-                request.FILES['play'],
+
+            self.play = Compiler(
+                request.FILES['play'].read(),
                 LANGUAGES[request.POST['play_language']],
-                partial(self.notify, 'play')
+                partial(self.notify, label='play')
             )
-            visualizer = Compiler(
-                request.FILES['visualizer'],
+
+            self.visualizer = Compiler(
+                request.FILES['visualizer'].read(),
                 LANGUAGES[request.POST['visualizer_language']],
-                partial(self.notify, 'visualizer')
+                partial(self.notify, label='visualizer')
             )
-            print(1)
-            ideal_solution.compile()
-            play.compile()
-            visualizer.compile()
-            print(2)
+            self.ideal_solution.compile()
+            self.play.compile()
+            self.visualizer.compile()
+            print(self.ideal_solution)
+            print(self.play_report)
             return render(request, self.template_name, {
-                'status': 'compilation',
+                'status': 'compiling',
                 'game_form': self.game_form,
-                'ideal_solution_report': self.ideal_solution_report,
-                'play_report': self.play_report,
-                'visualizer_report': self.visualizer_report,
+                'ideal_solution_report': self.ideal_solution.report,
+                'play_report': self.play.report,
+                'visualizer_report': self.visualizer.report,
             })
 
-        elif request.POST['type'] == 'compilation in progress':
-            print(3)
-            status = 'compilation'
-            if self.ideal_solution_status == self.play_status == self.visualizer_status == 'Done':
+        elif request.POST['type'] == 'compilation':
+            status = 'compiling'
+            if self.ideal_solution_report and self.play_report and self.visualizer_report:
                 status = 'receive compiler report'
 
+            print(self.ideal_solution)
+            print(self.play_report)
             return render(request, self.template_name, {
                 'status': status,
                 'game_form': self.game_form,
@@ -72,13 +85,10 @@ class GameUploadView(View):
                 'visualizer_report': self.visualizer_report,
             })
 
-    def notify(self, compiler_report, label):
+    def notify(self, report, label):
         if label == 'ideal_solution':
-            self.ideal_solution_report = compiler_report
-            self.ideal_solution_status = 'Done'
+            self.ideal_solution_report = report
         elif label == 'play':
-            self.play_report = compiler_report
-            self.play_status = 'Done'
+            self.play_report = report
         elif label == 'visualizer':
-            self.visualizer_report = compiler_report
-            self.visualizer_status = 'Done'
+            self.visualizer_report = report
