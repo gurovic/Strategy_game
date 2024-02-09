fe3bcef4fc84d500dc28c42405f6d04118320b55
Sasha G <a1exgo111v@gmail.com>
2024-02-09 16:55:09 +0300

    edit past_tournaments sorter

--- website/app/views/tournaments_view.py
+++ website/app/views/tournaments_view.py
@@ -6,9 +6,8 @@ from ..models import Tournament
 
 
 def show(request):
-    tournaments = Tournament.objects.order_by("start_time", "end_time")
-    upcoming_or_current_tournaments = tournaments.filter(end_time__gte=timezone.now())
-    past_tournaments = tournaments.filter(end_time__lt=timezone.now())
+    upcoming_or_current_tournaments = Tournament.objects.filter(end_time__gte=timezone.now()).order_by("start_time", "end_time")
+    past_tournaments = Tournament.objects.filter(end_time__lt=timezone.now()).order_by("-start_time", "end_time")
 
     return render(request, 'tournaments.html',
                   {'upcoming_or_current_tournaments': upcoming_or_current_tournaments, 'past_tournaments': past_tournaments})
