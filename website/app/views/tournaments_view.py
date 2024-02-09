7d62e469d97a141e8e76da2232182d383d78d78c
Sasha G <a1exgo111v@gmail.com>
2024-02-09 12:10:23 +0300

    edit tournaments view

--- website/app/views/tournaments_view.py
+++ website/app/views/tournaments_view.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
 
-from ..models import Tournament, PlayerInTournament
+from ..models import Tournament
 
 
 def show(request):
-    tournaments = Tournament.objects.all()
+    tournaments = Tournament.objects.order_by("-start_time", "end_time")
+    upcoming_or_current_tournaments = Tournament.objects.filter(end_time__gte=timezone.now()).order_by("-start_time", "end_time")
+    past_tournaments = Tournament.objects.filter(end_time__lt=timezone.now()).order_by("-start_time", "end_time")
 
-    return render(request, 'tournaments.html', {'tournaments':tournaments})
+    return render(request, 'tournaments.html',
+                  {'upcoming_or_current_tournaments': upcoming_or_current_tournaments, 'past_tournaments': past_tournaments})
