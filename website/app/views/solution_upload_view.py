197ffe225410e462a5340bdbdc5d1086b8aec6b6
Marike <Mariabessolova23836@gmail.com>
2024-02-10 12:45:46 +0300

    upload solution fixed

--- website/app/views/solution_upload_view.py
+++ website/app/views/solution_upload_view.py
@@ -3,16 +3,16 @@ from ..models.tournament import Tournament
 from ..models.player_in_tournament import PlayerInTournament
 
 def upload(request, tournament_id, user_id):
-    ourtournament = Tournament.objects.get(pk=tournament_id)
+    our_tournament = Tournament.objects.get(pk=tournament_id)
     if request.method == 'POST':
-        current_player_in_tournament = ourtournament.players.get(pk=user_id)
+        current_player_in_tournament = our_tournament.players.get(pk=user_id)
         current_player_in_tournament.file_solution = request.FILES['strategy'].read()
         return render(request, 'solution_upload.html', {'status': 'strategy uploaded'})
 
     else:
-        if ourtournament.Status == 'WAITING_SOLUTIONS':
+        if our_tournament.Status == 'WAITING_SOLUTIONS':
             return render(request, 'solution_upload.html', {'status': 'wait for strategy'})
-        elif ourtournament.Status == 'NOT_STARTED':
+        elif our_tournament.Status == 'NOT_STARTED':
             return render(request, 'solution_upload.html', {'status': 'not started'})
         else:
             return render(request, 'solution_upload.html', {'status': 'finished'})
