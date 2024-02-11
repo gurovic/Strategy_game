9677a08bad53de88f9779fd48173e936511174b9
Palov Dmitriy <palov.dmitriy@yandex.ru>
2024-02-11 18:26:15 +0300

    fixed tournament views (finally)

--- website/app/views/tournament_views_test.py
+++ website/app/views/tournament_views_test.py
@@ -7,27 +7,19 @@ from ..models.game import Game
 
 class TestTournamentViews(TestCase):
 
-    def test_if_form_is_valid(self):
-        mock_game = Mock()
-        data = {'name':'example_tournament', 'game':None, 'system':0, 'start_time':'2006-10-25 14:30:59', 'end_time':'2006-10-26 14:30:59', 'max_of_players':10}
-        #if request.method == "POST":
+    @classmethod
+    def setUpTestData(cls):
+        cls.game = Game.objects.create(id=0)
+
+    @patch('app.models.game')
+    def test_if_form_is_valid(self, mock_game):
+        data = {'name':'example_tournament', 'game':self.game, 'system':0, 'start_time':'2006-10-25 14:30:59', 'end_time':'2006-10-26 14:30:59', 'max_of_players':10}
         form = TournamentForm(data)
         if (form.is_valid()):
            response = self.client.get('/app/tournament/create/')
            self.assertEqual(response.status_code, 200)
         else:
             self.fail()
-
-        #else:
-        #    response = self.client.get('/app/tournament/create/')
-        #    self.assertEqual(response.status_code, 200)
-        #    self.assertTemplateUsed(response, 'tournament_create.html')
-        #else:
-        #    response = self.client.get('/app/tournament/create')
-        #    self.assertEqual(response.status_code, 200)
-        #    self.assertTemplateUsed(response, 'tournament_create.html')
-
-
 
     def test_correct_template_1(self):
         response = self.client.get('/app/tournament/')
