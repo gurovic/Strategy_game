641c03d5b859fcd9b6187acdd0aefa4affeb3848
boriskiva <boriskiva3@gmail.com>
2024-02-11 11:05:30 +0300

    making final tests

--- website/app/models/tournamentsystem_test.py
+++ website/app/models/tournamentsystem_test.py
@@ -1,39 +1,21 @@
 import unittest
-from unittest.mock import Mock, patch
 
 from django.contrib.auth.models import User
+from django.db import models
+from django.test import TestCase
 
 from .tournamentsystem import TournamentSystem
-from django.db import models
 
 
-class TestTournamentSystem(unittest.TestCase):
-    @patch('app.models.tournamentsystem.TournamentSystem')
-    def test_init(self, mock_players_in_tournament):
-        mock_players_in_tournament.return_value = Mock()
+class TestTournamentSystem(TestCase):
+    def test_init(self):
+        class A(TournamentSystem):
+            def __init__(self):
+                super().__init__()
 
-        class MockTournament(models.Model):
-            count = 0
+        a = A()
+        print(a)
 
-            def finish_tournament(self):
-                self.count += 1
-
-        mock_tournament = MockTournament()
-        tournament_system = TournamentSystem(mock_tournament)
-        self.assertIs(tournament_system.tournament, mock_tournament)
-
-    def test_finish(self):
-        class MockTournament:
-            count = 0
-            players = []
-
-            def finish_tournament(self):
-                self.count += 1
-
-        mock_tournament = MockTournament()
-        tournament_system = TournamentSystem(mock_tournament)
-        tournament_system.finish()
-        self.assertEqual(mock_tournament.count, 1)
 
 
 if __name__ == '__main__':
--- website/app/urls.py
+++ website/app/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from .views import sandbox_views, tournament_views, tournament_start_view, tournament_register_views, tournament_results_view, solution_upload_view
+from .views import tournament_finish_view
 
 urlpatterns = [
     path('sandbox/<int:game_id>', sandbox_views.show),
