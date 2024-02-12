ff7fabc725ddfc63f7d954285c5081d1b4903096
boriskiva <boriskiva3@gmail.com>
2024-02-12 12:20:16 +0300

    fixing start_time, end_time

--- website/app/views/tournaments_view.py
+++ website/app/views/tournaments_view.py
@@ -6,8 +6,8 @@ from app.models import Tournament
 
 
 def show(request):
-    upcoming_or_current_tournaments = Tournament.objects.filter(end_time__gte=timezone.now()).order_by("tournament_start_time", "finish_registration_time")
-    past_tournaments = Tournament.objects.filter(end_time__lt=timezone.now()).order_by("-tournament_start_time", "finish_registration_time")
+    upcoming_or_current_tournaments = Tournament.objects.filter(finish_registration_time__gte=timezone.now()).order_by("tournament_start_time", "finish_registration_time")
+    past_tournaments = Tournament.objects.filter(finish_registration_time__lt=timezone.now()).order_by("-tournament_start_time", "finish_registration_time")
 
     return render(request, 'tournaments.html',
                   {'upcoming_or_current_tournaments': upcoming_or_current_tournaments, 'past_tournaments': past_tournaments})
