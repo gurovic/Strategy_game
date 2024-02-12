272b57ac9f9159ce6b75484c7da58466be294bb8
MrNereof <mrnereof@gmail.com>
2024-02-12 16:00:49 +0300

    Fix DjangoQ config warning

--- website/website/settings.py
+++ website/website/settings.py
@@ -150,3 +150,14 @@ USE_DOCKER = False
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+
+Q_CLUSTER = {
+    'name': 'DjangORM',
+    'workers': 4,
+    'timeout': 90,
+    'retry': 120,
+    'queue_limit': 50,
+    'bulk': 10,
+    'orm': 'default'
+}
