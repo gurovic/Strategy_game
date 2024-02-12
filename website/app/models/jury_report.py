d6fb3e070606b721085364fbdcf054650d4fe5e2
NotAnArchitect <ddybroff@gmail.com>
2024-02-12 12:24:31 +0300

    fix

--- website/app/models/jury_report.py
+++ website/app/models/jury_report.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class JuryReport(models.Model):
-    story_of_game = models.TextField(editable=False, blank=True, null=True, verbose_name="Ход игры")
-    points = models.TextField(editable=False, blank=True, null=True, verbose_name="Очки игроков")
-    status = models.TextField(editable=False, blank=True, null=True, verbose_name="Статус боя")
+    story_of_game = models.Field(editable=False, blank=True, null=True, verbose_name="Ход игры")
+    points = models.ManyToManyField(editable=False, blank=True, null=True, verbose_name="Очки игроков")
+    status = models.IntegerField(editable=False, blank=True, null=True, verbose_name="Статус боя")
