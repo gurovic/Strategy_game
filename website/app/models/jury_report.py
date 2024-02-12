2e237b39df7eba39d9374736e268ec1c7da0c8be
NotAnArchitect <ddybroff@gmail.com>
2024-02-12 13:06:18 +0300

    fix

--- website/app/models/jury_report.py
+++ website/app/models/jury_report.py
@@ -4,4 +4,5 @@ from django.db import models
 class JuryReport(models.Model):
     story_of_game = models.Field(editable=False, blank=True, null=True, verbose_name="Ход игры")
     points = models.JSONField(editable=False, blank=True, null=True, verbose_name="Очки игроков")
-    status = models.IntegerField(editable=False, blank=True, null=True, verbose_name="Статус боя")
+    ORDER_STATUS = ((0, 'ERROR'), (1, 'OK'))
+    status = models.IntegerField(editable=False, blank=True, null=True, verbose_name="Статус боя", choices=ORDER_STATUS)
