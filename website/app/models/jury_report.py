400e0ec13226639b054a2ee2403ee540bf57d84b
NotAnArchitect <ddybroff@gmail.com>
2024-02-12 15:54:44 +0300

    fix

--- website/app/models/jury_report.py
+++ website/app/models/jury_report.py
@@ -5,6 +5,6 @@ class JuryReport(models.Model):
     class Status(models.IntegerChoices):
         OK = 1
         ERROR = 0
-    story_of_game = models.Field(editable=False, blank=True, null=True, verbose_name="Ход игры")
+    story_of_game = models.TextField(editable=False, blank=True, null=True, verbose_name="Ход игры")
     points = models.JSONField(editable=False, blank=True, null=True, verbose_name="Очки игроков")
     status = models.IntegerField(editable=False, blank=True, null=True, verbose_name="Статус боя", choices=Status.choices)
