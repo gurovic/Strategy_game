487880f1ddac650625c9ff9d281467301cca3249
MrNereof <mrnereof@gmail.com>
2024-02-15 09:41:08 +0300

    Move InvokerProcess to Invoker file due to circular import error

--- website/invoker/invoker.py
+++ website/invoker/invoker.py
@@ -8,6 +8,7 @@ from django.core.files import File as FileDjango
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from datetime import datetime
+from threading import Thread
 from pathlib import Path
 import subprocess
 import tempfile
@@ -37,6 +38,74 @@ class RunResult:
 
     input_files: typing.Optional[list[File]] = None
     preserved_files: typing.Optional[list[File]] = None
+
+
+class StdIn(typing.Protocol):
+    def write(self, data: str):
+        ...
+
+
+class StdOut(typing.Protocol):
+    def read(self) -> str:
+        ...
+
+    def readline(self) -> str:
+        ...
+
+
+class InvokerProcess(ABC):
+    stdin: StdIn
+    stdout: StdOut
+
+    def __init__(self, *args, label: typing.Optional[str] = None,
+                 preserve_files: typing.Optional[list[str]] = None,
+                 timelimit: typing.Optional[int] = None,
+                 callback: typing.Optional[typing.Callable[[RunResult], None]] = None,
+                 **kwargs):
+        self.label = label
+        self.preserve_files = preserve_files
+        self.timelimit = timelimit
+        self.callback = callback
+        self._run_result = None
+        super().__init__(*args, **kwargs)
+
+        if self.callback:
+            self.register_callback()
+
+    def register_callback(self):
+        Thread(target=self._wait_for_end).start()
+
+    @abstractmethod
+    def wait(self, timeout: typing.Optional[int] = None):
+        ...
+
+    @abstractmethod
+    def kill(self):
+        ...
+
+    def connect(self, input: str) -> str:
+        self.stdin.write(input)
+        return self.stdout.readline()
+
+    def _wait_for_end(self):
+        try:
+            self.wait(self.timelimit)
+        except subprocess.TimeoutExpired as exc:
+            self.kill()
+        self.send_callback()
+
+    def send_callback(self):
+        self.callback(self.run_result)
+
+    @abstractmethod
+    def make_run_result(self) -> RunResult:
+        ...
+
+    @property
+    def run_result(self) -> RunResult:
+        if not self._run_result:
+            self._run_result = self.make_run_result()
+        return self._run_result
 
 
 class InvokerEnvironment(ABC):
