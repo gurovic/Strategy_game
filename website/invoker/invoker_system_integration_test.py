7e1e9577978c7f21a28282b26fc1982425e571d8
Sasha G <a1exgo111v@gmail.com>
2024-02-21 12:33:23 +0300

    some edit

--- website/invoker/invoker_system_integration_test.py
+++ website/invoker/invoker_system_integration_test.py
@@ -1,28 +1,31 @@
 from unittest.mock import patch, Mock
 from django.test import TestCase
 
-from .invoker_request import InvokerRequest, InvokerRequestType
+from .invoker import Invoker
+from .invoker_request import InvokerRequest
+from .models import InvokerReport
 from .invoker_multi_request import InvokerMultiRequest, Priority
 from .invoker_multi_request_priority_queue import InvokerMultiRequestPriorityQueue
 
 
 class TestInvokerSystem(TestCase):
-    @patch('invoker.invoker_multi_request.InvokerMultiRequest.run')
+    @patch('invoker.invoker_multi_request.InvokerMultiRequest.notify')
     @patch('website.settings.MAX_INVOKERS_COUNT')
-    def test(self, mock_max_invoker_count: Mock, mock_imr_run: Mock):
+    def test(self, mock_max_invoker_count: Mock, mock_i_m_r_notify: Mock):
         mock_max_invoker_count.return_value(1)
+        mock_i_m_r_notify.return_value = InvokerMultiRequest.notify
 
         i_m_r_p_queue = InvokerMultiRequestPriorityQueue()
 
-        invoker_request1 = InvokerRequest('python ../libgame/examples/bolshe_menshe_data/bolshe_menshe_solution1.py')
-        invoker_multi_request1 = InvokerMultiRequest([invoker_request1], Priority.RED)
-
-        invoker_request2 = InvokerRequest('python ../libgame/examples/bolshe_menshe_data/bolshe_menshe_solution2.py')
-        invoker_multi_request2 = InvokerMultiRequest([invoker_request2], Priority.YELLOW)
-
-        i_m_r_p_queue.invoker_multi_request_queue.put(invoker_multi_request1)
-        i_m_r_p_queue.invoker_multi_request_queue.put(invoker_multi_request2)
+        command = 'python'
+        file = './test_solutions/solution1.py'
+        invoker_request1 = InvokerRequest(command=command, files=[file])
+        invoker_multi_request = InvokerMultiRequest([invoker_request1], Priority.RED)
+        i_m_r_p_queue.invoker_multi_request_queue.put(invoker_multi_request)
 
         i_m_r_p_queue.run()
 
-        mock_imr_run.assert_called()
+        mock_i_m_r_notify.assert_called()
+
+        print(mock_i_m_r_notify.call_args)
+
