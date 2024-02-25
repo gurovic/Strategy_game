77940860764cfd420434131090502162e1adbfaa
Misha Lichmanov <m.lichmanov@gmail.com>
2024-02-25 22:39:47 +0300

    jury changed to satisfy invoker_processes

--- website/app/classes/jury.py
+++ website/app/classes/jury.py
@@ -12,6 +12,9 @@ class GameState(enum.Enum):
 
 
 class Jury:
+
+    process = None
+
     def __init__(self, invoker_multi_request: InvokerMultiRequest):
         self.invoker_multi_request = invoker_multi_request
 
@@ -26,6 +29,10 @@ class Jury:
         self.game_state = GameState.PLAY
         self.jury_report = JuryReport()
 
+
+    def notify_processes(self, processes):
+        self.process = processes
+
     def get_invoker_requests(self):
         invoker_requests = self.invoker_multi_request.invoker_requests
         for invoker_request in invoker_requests:
@@ -35,9 +42,13 @@ class Jury:
                 self.strategies_invoker_requests.append(invoker_request)
 
     def get_processes(self):
-        self.play_process = self.play_invoker_request.process_callback
-        for invoker_request in self.strategies_invoker_requests:
-            self.strategies_process.append(invoker_request.process_callback)
+        invoker_requests = self.invoker_multi_request.invoker_requests
+        for i in range(len(self.process)):
+            invoker_request = invoker_requests[i]
+            if invoker_request.type == InvokerRequestType.PLAY:
+                self.play_process = self.process[i]
+            else:
+                self.strategies_process.append(self.process[i])
 
     def perform_play_command(self):
         try:
