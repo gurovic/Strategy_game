aecbdc39f1ddac2123c2f3c015c2e4e47b28955c
NotAnArchitect <ddybroff@gmail.com>
2024-02-25 14:24:51 +0300

    доисправление жюри репорта

--- website/app/models/jury_report.py
+++ website/app/models/jury_report.py
@@ -1,10 +1,12 @@
 from django.db import models
+from battle import Battle
 
 
 class JuryReport(models.Model):
     class Status(models.IntegerChoices):
         OK = 1
         ERROR = 0
+    battle = Battle()
     story_of_game = models.TextField(blank=True, null=True, verbose_name="Ход игры")
     points = models.JSONField(blank=True, null=True, verbose_name="Очки игроков")
     status = models.IntegerField(blank=True, null=True, verbose_name="Статус боя", choices=Status.choices)
