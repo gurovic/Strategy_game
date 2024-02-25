0ceaeba4ee0894bdef12bd58cdb57b6b8521234e
NotAnArchitect <ddybroff@gmail.com>
2024-02-25 15:08:52 +0300

    доисправление jury_report

--- website/app/models/jury_report.py
+++ website/app/models/jury_report.py
@@ -1,12 +1,12 @@
 from django.db import models
-from battle import Battle
+from .battle import Battle
 
 
 class JuryReport(models.Model):
     class Status(models.IntegerChoices):
         OK = 1
         ERROR = 0
-    battle = Battle()
+    battle = models.ForeignKey(Battle, on_delete=models.CASCADE, null=True)
     story_of_game = models.TextField(blank=True, null=True, verbose_name="Ход игры")
     points = models.JSONField(blank=True, null=True, verbose_name="Очки игроков")
     status = models.IntegerField(blank=True, null=True, verbose_name="Статус боя", choices=Status.choices)
--- website/app/models/jury_report_test.py
+++ website/app/models/jury_report_test.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from .battle import Battle
 from app.models.jury_report import JuryReport
 
 
@@ -8,8 +8,11 @@ class JuryReportTestCase(TestCase):
         story_of_game = "lol"
         points = {1: 1, 2: 1}
         status = 1
-        jury_report = JuryReport.objects.create(story_of_game=story_of_game, points=points, status=status)
-
+        battle = Battle(status="O")
+        battle.save()
+        jury_report = JuryReport.objects.create(battle=battle, story_of_game=story_of_game, points=points, status=status)
+        jury_report.save()
+        self.assertEquals(battle.status, jury_report.battle.status)
         self.assertEquals(story_of_game, str(jury_report.story_of_game))
         self.assertEquals(str(points), str(jury_report.points))
         self.assertEquals(status, jury_report.status)
