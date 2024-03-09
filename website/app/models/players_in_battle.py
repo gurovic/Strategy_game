592eaaea62b2d99cf4b78bb247986f68c672ee15
Sasha G <a1exgo111v@gmail.com>
2024-03-09 11:14:12 +0300

    edit points type

--- website/app/models/players_in_battle.py
+++ website/app/models/players_in_battle.py
@@ -7,6 +7,6 @@ class PlayersInBattle(models.Model):
     battle = models.ForeignKey("Battle", on_delete=models.CASCADE, null=True)
     player = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
     number = models.IntegerField(default=0, verbose_name='Номер в батле')
-    number_of_points = models.IntegerField(default=0, verbose_name='number of point')
+    number_of_points = models.FloatField(default=0, verbose_name='number of point')
     place = models.IntegerField(default=0, verbose_name='place')
     file_solution = models.FileField(upload_to='player_in_battle_files', null=True, verbose_name='file')
