2947e78e40a2c08b36f03dbed76acc41fa92ce48
Marike <Mariabessolova23836@gmail.com>
2024-03-11 23:44:39 +0300

    должно работать

--- website/app/models/game.py
+++ website/app/models/game.py
@@ -3,18 +3,18 @@ from django.db import models
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return "user_{0}/{1}".format(instance.user.id, filename)
+    return "game_{0}/{1}".format(instance.Game.name, filename)
 
 
 class Game(models.Model):
     name = models.CharField(max_length=50, default='')
     number_of_players = models.IntegerField(default=0, verbose_name="Number of players")
-    ideal_solution = models.FileField(upload_to='games_files/', null=True, verbose_name='Ideal strategy')
-    play = models.FileField(upload_to='games_files/', null=True, verbose_name='Play')
+    ideal_solution = models.FileField(upload_to=user_directory_path, null=True, verbose_name='Ideal strategy')
+    play = models.FileField(upload_to=user_directory_path, null=True, verbose_name='Play')
     win_point = models.IntegerField(default=0, null=True, blank=True, verbose_name='Point for winner')
     lose_point = models.IntegerField(default=0, null=True, blank=True, verbose_name='Point for loser')
-    visualiser = models.FileField(upload_to='games_files/', null=True, blank=True, verbose_name='Visualiser')
-    rules = models.FileField(upload_to='games_files/', null=True, blank=True, verbose_name='Rules')
+    visualiser = models.FileField(upload_to=user_directory_path, null=True, blank=True, verbose_name='Visualiser')
+    rules = models.FileField(upload_to=user_directory_path, null=True, blank=True, verbose_name='Rules')
 
     def __str__(self):
         return f"{self.name}"
