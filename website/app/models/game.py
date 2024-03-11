4e833edfd6d56d50990ef6118500f5e94356f6ed
Marike <Mariabessolova23836@gmail.com>
2024-03-12 00:00:53 +0300

    должно работать

--- website/app/models/game.py
+++ website/app/models/game.py
@@ -3,7 +3,7 @@ from django.db import models
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return "game_{0}/{1}".format(instance.Game.name, filename)
+    return "game_{0}/{1}".format(instance.name, filename)
 
 
 class Game(models.Model):
