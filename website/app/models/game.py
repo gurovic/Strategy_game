3cb5e16c36664499c314ebfb087a38ae1e1a5204
Marike <Mariabessolova23836@gmail.com>
2024-03-11 15:54:32 +0300

    new

--- website/app/models/game.py
+++ website/app/models/game.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return "user_{0}/{1}".format(instance.user.id, filename)
+
+
 class Game(models.Model):
     name = models.CharField(max_length=50, default='')
     number_of_players = models.IntegerField(default=0, verbose_name="Number of players")
