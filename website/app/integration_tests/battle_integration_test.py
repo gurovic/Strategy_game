c0edd6eff0d6da897f138b87f966ce06b41ec697
Ilgizar <instasamka@gmail.com>
2024-03-19 14:34:38 +0300

    Int. Test: Battle - 1

--- website/app/integration_tests/battle_integration_test.py
+++ website/app/integration_tests/battle_integration_test.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from ..classes.jury import Jury
+from ..launcher import Launcher
 from ..models import Battle
 from ...invoker.invoker_multi_request import InvokerMultiRequest
 from ...invoker.invoker_request import InvokerRequest
@@ -9,9 +10,12 @@ from ...invoker.invoker_request import InvokerRequest
 class BattleTest(TestCase):
     def BattleRunTest(self):
         battle = Battle()
-        first_strategy_invoker_request = InvokerRequest("")
-        second_strategy_invoker_request = InvokerRequest("")
-        play_inventory_request = InvokerRequest("")
+        file_path1 = "battle_test_files/strategy1"
+        file_path2 = "battle_test_files/strategy2"
+        file_path3 = "battle_test_files/play"
+        first_strategy_invoker_request = InvokerRequest(f'python {file_path1}')
+        second_strategy_invoker_request = InvokerRequest(f'python {file_path2}')
+        play_inventory_request = InvokerRequest(f'python {file_path3}')
         invoker_multi_request = InvokerMultiRequest(
             [first_strategy_invoker_request, second_strategy_invoker_request, play_inventory_request])
         jury = Jury(invoker_multi_request)
