5850d8a49db5a5ac0ed71465fe28e12454529214
Ilgizar <instasamka@gmail.com>
2024-03-27 18:55:41 +0300

    Int. Test:Compiler & jury - 4

--- website/app/integration_tests/compiler_and_jury_intagration_test.py
+++ website/app/integration_tests/compiler_and_jury_intagration_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+from invoker.invoker import NormalProcess
 from invoker.invoker_multi_request_priority_queue import InvokerMultiRequestPriorityQueue
 from invoker.invoker_request import InvokerRequest
 from invoker.invoker_request import InvokerRequestType
@@ -9,6 +10,7 @@ from app.classes.jury import GameState
 from app.classes.jury import Jury
 from app import compiler
 import time
+import subprocess
 
 from app import launcher
 
@@ -47,19 +49,29 @@ class Test(unittest.TestCase):
         launcher_strategy1 = launcher.Launcher(solution_first_compiled_path)
         launcher_strategy2 = launcher.Launcher(solution_second_compiled_path)
 
-        IR_play = InvokerRequest(launcher_play.command(), [play_compiled_path])
+        subprocess_play = subprocess.Popen(play_compiled_path)
+        subprocess_strategy1 = subprocess.Popen(solution_first_compiled_path)
+        subprocess_strategy2 = subprocess.Popen(solution_second_compiled_path)
+
+        play_process = NormalProcess(subprocess_play, label="play")
+        strategy_process_1 = NormalProcess(subprocess_strategy1, label="player1")
+        strategy_process_2 = NormalProcess(subprocess_strategy2, label="player2")
+
+        IR_play = InvokerRequest(launcher_play.command(), [play_compiled_path], process_callback=play_process)
         IR_play.label = "play"
-        IR_sol1 = InvokerRequest(launcher_strategy1.command(), [solution_first_compiled_path])
+        IR_sol1 = InvokerRequest(launcher_strategy1.command(), [solution_first_compiled_path],
+                                 process_callback=strategy_process_1)
         IR_sol1.label = "strategy"
-        IR_sol2 = InvokerRequest(launcher_strategy2.command(), [solution_second_compiled_path])
+        IR_sol2 = InvokerRequest(launcher_strategy2.command(), [solution_second_compiled_path],
+                                 process_callback=strategy_process_2)
         IR_sol2.label = "strategy"
 
         IM_process_of_battle = InvokerMultiRequest([IR_play, IR_sol1, IR_sol2], Priority.RED)
         jury_of_battle = Jury(IM_process_of_battle)
 
         IM_process_of_battle.subscribe(jury_of_battle)
-        IM_process_of_battle.start()
-        time.sleep(1)
+        IM_process_of_battle.send_process()
 
         jury_of_battle.perform_play_command()
+
         self.assertEqual(jury_of_battle.game_state, GameState.PLAY)
