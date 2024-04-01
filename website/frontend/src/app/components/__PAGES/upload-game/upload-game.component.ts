5d702fcb312e153dd2792e039b8041386fbd9e12
Marike <Mariabessolova23836@gmail.com>
2024-04-01 09:41:13 +0300

    не работает

---
+++ website/frontend/src/app/components/__PAGES/upload-game/upload-game.component.ts
@@ -0,0 +1,57 @@
+import {Component, OnInit} from '@angular/core';
+import {RegisterApiService} from "../../../services/api/register-api.service";
+import {UserRegister} from "../../../models/api/user-register.model";
+import {Router} from "@angular/router";
+import {ProfileService} from "../../../services/profile.service";
+import {ProfileApiService} from "../../../services/api/profile-api.service";
+
+@Component({
+    selector: 'app-upload-game',
+    templateUrl: './upload-game.component.html',
+    styleUrls: ['./upload-game.component.scss']
+})
+export class UploadGameComponent implements OnInit {
+    public user: UserRegister = {username: '', password1: '', email: '', password2: ''};
+
+    constructor(
+        public register_service: RegisterApiService,
+        private profile_service: ProfileService,
+        private profile_api_service: ProfileApiService,
+        private router: Router,
+    ) {
+    }
+
+    ngOnInit(): void {
+    }
+
+    register() {
+        this.register_service.create(this.user).subscribe(
+            resp => {
+                this.profile_api_service.get().subscribe(
+                    response => {
+                        this.profile_service.set_user(response);
+                        setTimeout(() => {
+                            // @ts-ignore
+                            document.getElementById('loader-box').style.display = 'none';
+                            this.router.navigate(['profile'])
+                            location.reload();
+                        }, 1000)
+                    },
+                    error => {
+                        // @ts-ignore
+                        document.getElementById('loader-box').style.display = 'none';
+                    },
+                )
+            },
+            error => {
+                // @ts-ignore
+                document.getElementById('loader-box').style.display = 'none';
+            },
+        )
+    }
+
+    go_link(link: string) {
+        this.router.navigate([link]);
+    }
+
+}
