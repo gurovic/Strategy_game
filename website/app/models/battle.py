cad699ef42721a0af0cd703c9d508df92e0571b7
DerwesX <2025lobanov.ss@student.letovo.ru>
2024-04-10 13:20:45 +0300

    compiled_file_adding_i_guess?

--- website/app/models/battle.py
+++ website/app/models/battle.py
@@ -50,42 +50,46 @@ class Battle(models.Model):
 
         file = os.path.join(MEDIA_ROOT, str(self.game.play.path))
         list_compiled_file = []
-        if file.split(".")[-1][0] != 'e':
-            play_compiled = CompiledFile()
-            list_compiled_file.append(play_compiled)
-            Compiler(file, file.split(".")[-1], play_compiled.get_compiled_file).compile()
-            file = os.path.join(MEDIA_ROOT, str(play_compiled.compiled_file))
 
-        launcher = Launcher(os.path.abspath(str(file)), label="play")
-        requests.append(launcher)
-        players_in_battle = PlayersInBattle.objects.filter(battle=self)
-        number = 0
-        files_list = []
-        for player_in_battle in players_in_battle:
-            number += 1
-            self.numbers[number] = player_in_battle.player
-            file = player_in_battle.file_solution.path
-            if file.split(".")[-1][0] != 'e':
-                strategy_compiled = CompiledFile()
-                Compiler(file, file.split(".")[-1], strategy_compiled.get_compiled_file).compile()
-                list_compiled_file.append(strategy_compiled)
-                file = os.path.join(MEDIA_ROOT, str(strategy_compiled.compiled_file))
+        def get_compiled_file(compiler_report):
+            play_compiled_file = os.path.join(MEDIA_ROOT, str(compiler_report.compiled_file))
+            self.game.play = compiler_report.compiled_file
 
-            files_list.append(file)
-
-        while ok_sum != len(list_compiled_file):
-            continue
-
-        number = 1
-        for file in files_list:
-            launcher = Launcher(file, label=f'player{number}')
+            launcher = Launcher(os.path.abspath(str(play_compiled_file)), label="play")
             requests.append(launcher)
-            number += 1
+            players_in_battle = PlayersInBattle.objects.filter(battle=self)
+            number = 0
+            files_list = []
 
-        multi_request = InvokerMultiRequest(requests, priority=Priority.RED)
-        self.jury = Jury(multi_request)
-        multi_request.subscribe(self.jury)
-        multi_request.start()
+            for player_in_battle in players_in_battle:
+                number += 1
+                self.numbers[number] = player_in_battle.player
+                player_solution_file = player_in_battle.file_solution.path
+                if player_solution_file.split(".")[-1][0] != 'e':
+                    strategy_compiled = CompiledFile()
+                    Compiler(player_solution_file, player_solution_file.split(".")[-1], strategy_compiled.get_compiled_file).compile()
+                    list_compiled_file.append(strategy_compiled)
+                    player_solution_file = os.path.join(MEDIA_ROOT, str(strategy_compiled.compiled_file))
+
+                files_list.append(player_solution_file)
+
+            while ok_sum != len(list_compiled_file):
+                continue
+
+            number = 1
+            for compiled_file in files_list:
+                launcher = Launcher(compiled_file, label=f'player{number}')
+                requests.append(launcher)
+                number += 1
+
+            multi_request = InvokerMultiRequest(requests, priority=Priority.RED)
+            self.jury = Jury(multi_request)
+            multi_request.subscribe(self.jury)
+            multi_request.start()
+
+        # <--------- AFTER FUNCTION --------->
+        if file.split(".")[-1][0] != 'e':
+            Compiler(file, file.split(".")[-1], get_compiled_file).compile()
 
     def run(self):
         self.create_invoker_requests()
