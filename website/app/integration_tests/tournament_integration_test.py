48bbfc35069ab59d97c578ab2369b27eb6c8ec55
Ilgizar <instasamka@gmail.com>
2024-04-15 15:33:58 +0300

    test fix 1

--- website/app/integration_tests/tournament_integration_test.py
+++ website/app/integration_tests/tournament_integration_test.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.files import File
 
+from .. import compiler, launcher
 from ..classes.tests_utils import compile_and_upload_to_file_field
 from app.models import Game, Tournament, PlayerInTournament
 from app.compiler import Compiler, CompilerReport
@@ -13,8 +14,27 @@ from app.compiler import Compiler, CompilerReport
 class TestTournament(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.game = Game.objects.create(name="Some game", number_of_players=2, win_point=1, lose_point=0)
-        compile_and_upload_to_file_field(cls.game.play, "app/integration_tests/tournament_integration_test/game_play.py", 'py')
+        global ok_sum
+        ok_sum = 0
+
+        class CompiledFile:
+            ok_sum = 0
+
+            def get_compiled_file(self, compiler_report):
+                self.compiled_file = compiler_report.compiled_file
+                global ok_sum
+                ok_sum += 1
+
+        file = "app/integration_tests/tournament_integration_test/game_play.py"
+        compiled_play = CompiledFile()
+        compiler.Compiler(file, "py", compiled_play.get_compiled_file).compile()
+
+        launcher_play = launcher.Launcher(file, "py")
+
+        cls.game = Game.objects.create(name="Some game", number_of_players=2, win_point=1, lose_point=0,
+                                       play=compiled_play.compiled_file)
+        compile_and_upload_to_file_field(cls.game.play,
+                                         "app/integration_tests/tournament_integration_test/game_play.py", 'py')
 
         cls.user1 = User.objects.create_user(username='user1', password='12345')
         cls.user2 = User.objects.create_user(username='user2', password='12345')
@@ -24,15 +44,15 @@ class TestTournament(TestCase):
 
         player_in_tournament1 = PlayerInTournament.objects.create(player=cls.user1, tournament=cls.tournament)
         compile_and_upload_to_file_field(player_in_tournament1.file_solution,
-                                "app/integration_tests/tournament_integration_test/solution1.py", 'py')
+                                         "app/integration_tests/tournament_integration_test/solution1.py", 'py')
 
         player_in_tournament2 = PlayerInTournament.objects.create(player=cls.user2, tournament=cls.tournament)
         compile_and_upload_to_file_field(player_in_tournament2.file_solution,
-                                "app/integration_tests/tournament_integration_test/solution2.py", 'py')
+                                         "app/integration_tests/tournament_integration_test/solution2.py", 'py')
 
         player_in_tournament3 = PlayerInTournament.objects.create(player=cls.user3, tournament=cls.tournament)
         compile_and_upload_to_file_field(player_in_tournament3.file_solution,
-                                "app/integration_tests/tournament_integration_test/solution3.py", 'py')
+                                         "app/integration_tests/tournament_integration_test/solution3.py", 'py')
 
     @classmethod
     def tearDownClass(cls):
