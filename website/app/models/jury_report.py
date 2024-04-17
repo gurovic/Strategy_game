6bc807ff08dd9717e52a3ee3335a96bef5e12906
MrNereof <mrnereof@gmail.com>
2024-04-17 16:02:49 +0300

    Add display for JuryReport

--- website/app/models/jury_report.py
+++ website/app/models/jury_report.py
@@ -8,3 +8,8 @@ class JuryReport(models.Model):
     story_of_game = models.TextField(blank=True, null=True, verbose_name="Ход игры")
     points = models.JSONField(blank=True, null=True, verbose_name="Очки игроков")
     status = models.IntegerField(blank=True, null=True, verbose_name="Статус боя", choices=Status.choices)
+
+    def __str__(self):
+        if self.status == self.Status.OK:
+            return f"{self.get_status_display()} - {self.points}"
+        return self.get_status_display()
