94cb41c5ae62df58993ec3ff88388864e0ebed7e
Marike <Mariabessolova23836@gmail.com>
2024-04-17 13:20:53 +0300

    что-то там

--- website/app/models/game.py
+++ website/app/models/game.py
@@ -8,14 +8,13 @@ def user_directory_path(instance, filename):
 
 
 class Game(models.Model):
-Model
     name = models.CharField(max_length=50, default='')
     number_of_players = models.IntegerField(default=0, verbose_name="Number of players")
     ideal_solution = models.FileField(upload_to=user_directory_path, null=True, verbose_name='Ideal strategy')
     play = models.FileField(upload_to=user_directory_path, null=True, verbose_name='Play')
     win_point = models.IntegerField(default=0, null=True, blank=True, verbose_name='Point for winner')
     lose_point = models.IntegerField(default=0, null=True, blank=True, verbose_name='Point for loser')
-    visualiser = models.FileField(user_directory_pathload_to=user_directory_path, null=True, blank=True, verbose_name='Visualiser')
+    #visualiser = models.FileField(user_directory_pathload_to=user_directory_path, null=True, blank=True, verbose_name='Visualiser')
     rules = models.FileField(upload_to=user_directory_path, null=True, blank=True, verbose_name='Rules')
 
     def __init__(self):
