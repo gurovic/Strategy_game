7988835504fae2340563ddaa4494fc8494690cf5
Ilgizar <instasamka@gmail.com>
2024-04-17 13:03:47 +0300

    test fix 2

--- website/app/integration_tests/tournament_integration_test.py
+++ website/app/integration_tests/tournament_integration_test.py
@@ -14,27 +14,8 @@ from app.compiler import Compiler, CompilerReport
 class TestTournament(TestCase):
     @classmethod
     def setUpTestData(cls):
-        global ok_sum
-        ok_sum = 0
-
-        class CompiledFile:
-            ok_sum = 0
-
-            def get_compiled_file(self, compiler_report):
-                self.compiled_file = compiler_report.compiled_file
-                global ok_sum
-                ok_sum += 1
-
-        file = "app/integration_tests/tournament_integration_test/game_play.py"
-        compiled_play = CompiledFile()
-        compiler.Compiler(file, "py", compiled_play.get_compiled_file).compile()
-
-        launcher_play = launcher.Launcher(file, "py")
-
-        cls.game = Game.objects.create(name="Some game", number_of_players=2, win_point=1, lose_point=0,
-                                       play=compiled_play.compiled_file)
-        compile_and_upload_to_file_field(cls.game.play,
-                                         "app/integration_tests/tournament_integration_test/game_play.py", 'py')
+        cls.game = Game.objects.create(pk=1, play=os.path.abspath(
+            'app/integration_tests/tournament_integration_test/game_play.py'))
 
         cls.user1 = User.objects.create_user(username='user1', password='12345')
         cls.user2 = User.objects.create_user(username='user2', password='12345')
@@ -42,23 +23,20 @@ class TestTournament(TestCase):
 
         cls.tournament = Tournament.objects.create(name="Some tournament", game=cls.game)
 
-        player_in_tournament1 = PlayerInTournament.objects.create(player=cls.user1, tournament=cls.tournament)
-        compile_and_upload_to_file_field(player_in_tournament1.file_solution,
-                                         "app/integration_tests/tournament_integration_test/solution1.py", 'py')
+        PlayerInTournament.objects.create(player=cls.user1, tournament=cls.tournament,
+                                          file_solution='app/integration_tests/tournament_integration_test/game_play.py')
 
-        player_in_tournament2 = PlayerInTournament.objects.create(player=cls.user2, tournament=cls.tournament)
-        compile_and_upload_to_file_field(player_in_tournament2.file_solution,
-                                         "app/integration_tests/tournament_integration_test/solution2.py", 'py')
+        PlayerInTournament.objects.create(player=cls.user2, tournament=cls.tournament,
+                                          file_solution='app/integration_tests/tournament_integration_test/game_play.py')
 
-        player_in_tournament3 = PlayerInTournament.objects.create(player=cls.user3, tournament=cls.tournament)
-        compile_and_upload_to_file_field(player_in_tournament3.file_solution,
-                                         "app/integration_tests/tournament_integration_test/solution3.py", 'py')
+        PlayerInTournament.objects.create(player=cls.user3, tournament=cls.tournament,
+                                          file_solution='app/integration_tests/tournament_integration_test/game_play.py')
 
     @classmethod
     def tearDownClass(cls):
-        os.remove(cls.game.play.path)
+        cls.game.play.delete()
         for player_in_tournament in PlayerInTournament.objects.all():
-            os.remove(player_in_tournament.file_solution.path)
+            player_in_tournament.file_solution.delete()
         super().tearDownClass()
 
     def test(self):
