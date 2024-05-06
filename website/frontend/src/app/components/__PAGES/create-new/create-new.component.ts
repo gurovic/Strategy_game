4d6a0f50d1531ec59880040c8d4d807c5d2a2318
boriskiva <boriskiva3@gmail.com>
2024-05-06 19:54:36 +0300

    #454

--- website/frontend/src/app/components/__PAGES/create-new/create-new.component.ts
+++ website/frontend/src/app/components/__PAGES/create-new/create-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-new',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateNewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      public router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
